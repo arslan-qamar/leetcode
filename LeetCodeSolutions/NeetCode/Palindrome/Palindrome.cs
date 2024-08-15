e93815e11f0dd7df164bdfe232523d60265c767f
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-15 14:15:26 +1000

    10/150 done

---
+++ LeetCodeSolutions/NeetCode/Palindrome/Palindrome.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodeSolutions.NeetCode.Palindrome
+{
+    public class Palindrome
+    {
+        public static bool IsPalindrome(string s)
+        {
+            if (s == null)
+            {
+                return false;
+            }
+
+            int right = s.Length - 1;
+
+            for (int left = 0; left < right; left++, right--)
+            {
+
+                while (!IsValid(s[left]) && left < right)
+                {
+                    left++;
+                }
+
+                while (!IsValid(s[right]) && right > left)
+                {
+                    right--;
+                }
+
+                if (s[left].ToString().ToLower() != s[right].ToString().ToLower())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsValid(char c)
+        {
+            return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Palindrome/PalindromeTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Palindrome.Tests
+{
+    [TestClass()]
+    public class PalindromeTests
+    {
+        [TestMethod()]
+        public void IsPalindromeTest()
+        {
+            Palindrome.IsPalindrome("Was it a car or a cat I saw?").Should().BeTrue();
+            Palindrome.IsPalindrome("tab a cat").Should().BeFalse();
+            Palindrome.IsPalindrome("").Should().BeTrue();
+            Palindrome.IsPalindrome(" ").Should().BeTrue();
+            Palindrome.IsPalindrome("  ").Should().BeTrue();
+            Palindrome.IsPalindrome(null).Should().BeFalse();
+        }
+    }
+}
