cfbc48909ee9369255f73124e930493159df9314
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-15 12:06:32 +1000

    9/150 done

---
+++ LeetCodeSolutions/NeetCode/Longest Consecutive Sequence/LongestConsecutiveNumber.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCodeSolutions.NeetCode.Longest_Consecutive_Sequence
+{
+    public class LongestConsecutiveNumber
+    {
+        public static int LongestConsecutive(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            HashSet<int> numSet = GetNumSet(nums);
+
+            int[] sequenceStarts = FindSequences(nums, numSet);
+
+            int maxSequence = 1;
+            foreach (int sequenceStart in sequenceStarts)
+            {
+                int currentMax = 1;
+                int nextValue = sequenceStart + 1;
+                while (numSet.Contains(nextValue))
+                {
+                    currentMax++;
+                    if (currentMax > maxSequence)
+                    {
+                        maxSequence = currentMax;
+                    }
+
+                    nextValue++;
+                }
+            }
+
+            return maxSequence;
+        }
+
+        private static HashSet<int> GetNumSet(int[] nums)
+        {
+            HashSet<int> numSet = new HashSet<int>(nums);
+            return numSet;
+        }
+
+        private static int[] FindSequences(int[] nums, HashSet<int> numSet)
+        {
+            List<int> sequence = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int number = nums[i];
+                if (!numSet.Contains(number - 1))
+                {
+                    sequence.Add(number);
+                }
+            }
+
+            return sequence.ToArray();
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Longest Consecutive Sequence/LongestConsecutiveNumberTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Longest_Consecutive_Sequence.Tests
+{
+    [TestClass()]
+    public class LongestConsecutiveNumberTests
+    {
+        [TestMethod()]
+        public void LongestConsecutiveTest()
+        {
+            LongestConsecutiveNumber.LongestConsecutive(new int[] { 2, 20, 4, 10, 3, 4, 5 }).Should().Be(4);
+            LongestConsecutiveNumber.LongestConsecutive(new int[] { 0, 3, 2, 5, 4, 6, 1, 1 }).Should().Be(7);
+            LongestConsecutiveNumber.LongestConsecutive(new int[] { 0, 0, 0, 0, 0, 0 }).Should().Be(1);
+            LongestConsecutiveNumber.LongestConsecutive(new int[] { 0, 0, 0, 0, 1, 0 }).Should().Be(2);
+            LongestConsecutiveNumber.LongestConsecutive(new int[] { }).Should().Be(0);
+            LongestConsecutiveNumber.LongestConsecutive(null).Should().Be(0);
+        }
+    }
+}
