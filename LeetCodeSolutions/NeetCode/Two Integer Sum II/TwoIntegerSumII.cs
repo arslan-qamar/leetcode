82f55dc55b052f77e6937fad28829639373cf89d
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-15 16:55:39 +1000

    11/150 done

---
+++ LeetCodeSolutions/NeetCode/Two Integer Sum II/TwoIntegerSumII.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeSolutions.NeetCode.Two_Integer_Sum_II
+{
+    public class TwoIntegerSumII
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int[] result = new int[2];
+
+            int sum;
+
+            for (int l = 0, r = numbers.Length - 1; l < numbers.Length && l < r;)
+            {
+
+                sum = numbers[l] + numbers[r];
+
+                if (sum == target)
+                {
+                    return new int[] { l + 1, r + 1 };
+                }
+
+                if (sum < target)
+                {
+                    l++;
+                    continue;
+                }
+
+                if (sum > target)
+                {
+                    r--;
+                    continue;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Two Integer Sum II/TwoIntegerSumIITests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Two_Integer_Sum_II.Tests
+{
+    [TestClass()]
+    public class TwoIntegerSumIITests
+    {
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            TwoIntegerSumII.TwoSum(new int[] { 1, 2, 3, 4 }, 3).Should().BeEquivalentTo(new int[] { 1, 2 }, options => options.WithStrictOrdering());
+            TwoIntegerSumII.TwoSum(new int[] { }, 3).Should().BeEquivalentTo(new int[] { 0, 0 }, options => options.WithStrictOrdering());
+            TwoIntegerSumII.TwoSum(new int[] { 1, 2, 3, 4 }, 100).Should().BeEquivalentTo(new int[] { 0, 0 }, options => options.WithStrictOrdering());
+            TwoIntegerSumII.TwoSum(new int[] { 1, 2, 3, 100 }, 100).Should().BeEquivalentTo(new int[] { 0, 0 }, options => options.WithStrictOrdering());
+            TwoIntegerSumII.TwoSum(new int[] { -1, 2, 3, 101 }, 100).Should().BeEquivalentTo(new int[] { 1, 4 }, options => options.WithStrictOrdering());
+            TwoIntegerSumII.TwoSum(new int[] { -1, 2, 3, 101 }, 5).Should().BeEquivalentTo(new int[] { 2, 3 }, options => options.WithStrictOrdering());
+        }
+    }
+}
