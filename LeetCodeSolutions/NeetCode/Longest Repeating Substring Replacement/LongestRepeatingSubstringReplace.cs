71a55a9d838a4d19536b9ea18d1d00514e3cc276
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-17 09:53:18 +1000

    Long Repeating substring with Replacement

---
+++ LeetCodeSolutions/NeetCode/Longest Repeating Substring Replacement/LongestRepeatingSubstringReplace.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCodeSolutions.NeetCode.Longest_Repeating_Substring_Replacement
+{
+    public class LongestRepeatingSubstringReplace
+    {
+
+        public static int CharacterReplacement(string s, int k)
+        {
+            if (s == null || s.Length == 0)
+            {
+                return 0;
+            }
+
+            int longestSubString = 0;
+
+            int l = 0;
+
+            Dictionary<char, int> countMap = new Dictionary<char, int>();
+
+            for (int r = 0; r < s.Length; r++)
+            {
+                countMap[s[r]] = countMap.TryGetValue(s[r], out int curr) ? curr + 1 : 1;
+
+                int maxCount = FindMaxCount(countMap);
+
+                int windowSize = r - l + 1;
+
+                if (windowSize - maxCount <= k)
+                {
+
+                    longestSubString = Math.Max(longestSubString, windowSize);
+                }
+
+                while (windowSize - maxCount > k)
+                {
+                    countMap[s[l]] -= 1;
+                    l++;
+                    windowSize = r - l + 1;
+                    maxCount = FindMaxCount(countMap);
+                }
+            }
+
+            return longestSubString;
+        }
+
+        private static int FindMaxCount(Dictionary<char, int> countMap)
+        {
+            int maxCount = 0;
+            foreach (KeyValuePair<char, int> item in countMap)
+            {
+                if (item.Value > maxCount)
+                {
+                    maxCount = item.Value;
+                }
+            }
+
+            return maxCount;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Longest Repeating Substring Replacement/LongestRepeatingSubstringReplaceTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Longest_Repeating_Substring_Replacement.Tests
+{
+    [TestClass()]
+    public class LongestRepeatingSubstringReplaceTests
+    {
+        [TestMethod()]
+        public void CharacterReplacementTest()
+        {
+            LongestRepeatingSubstringReplace.CharacterReplacement("XYYX", 2).Should().Be(4);
+
+            LongestRepeatingSubstringReplace.CharacterReplacement("AAABABB", 1).Should().Be(5);
+
+        }
+    }
+}
