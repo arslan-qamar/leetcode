08e3f4068c930362641ec4dbc4cde6eb2f68c64d
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-16 17:55:18 +1000

    13/150

---
+++ LeetCodeSolutions/NeetCode/Trapping Rain Water/TrappingRainWater.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeSolutions.NeetCode.Trapping_Rain_Water
+{
+    public class TrappingRainWater
+    {
+        public static int Trap(int[] height)
+        {
+            int result = 0;
+            if (height == null || height.Length < 1)
+                return result;
+
+            int l = 0;
+            int r = height.Length - 1;
+
+            int lMax = height[l];
+            int rMax = height[r];
+
+            while (l < r)
+            {
+
+                if (lMax < rMax)
+                {
+                    l++;
+                    lMax = Math.Max(lMax, height[l]);
+                    result += lMax - height[l];
+                }
+
+                else
+                {
+                    r--;
+                    rMax = Math.Max(rMax, height[r]);
+                    result += rMax - height[r];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Trapping Rain Water/TrappingRainWaterTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Trapping_Rain_Water.Tests
+{
+    [TestClass()]
+    public class TrappingRainWaterTests
+    {
+        [TestMethod()]
+        public void TrapTest()
+        {
+            TrappingRainWater.Trap(new int[] { 0, 2, 0, 3, 1, 0, 1, 3, 2, 1 }).Should().Be(9);
+        }
+    }
+}
