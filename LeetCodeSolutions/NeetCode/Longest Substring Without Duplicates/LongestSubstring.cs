5fdfb2f84b147f5809854aa155fd9b00c29eb398
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-17 07:59:42 +1000

    Longest substring without duplicates.

---
+++ LeetCodeSolutions/NeetCode/Longest Substring Without Duplicates/LongestSubstring.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeSolutions.NeetCode.Longest_Substring_Without_Duplicates
+{
+    public class LongestSubstring
+    {
+
+        public static int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length < 1)
+            {
+                return 0;
+            }
+
+            int longestSubString = 0;
+
+            int l = 0;
+
+            HashSet<int> duplicateSet = new HashSet<int>();
+
+            for (int r = 0; r < s.Length; r++)
+            {
+                while (duplicateSet.Contains(s[r]))
+                {
+                    duplicateSet.Remove(s[l]);
+                    l++;
+                }
+
+                duplicateSet.Add(s[r]);
+
+                longestSubString = Math.Max(longestSubString, r - l + 1);
+            }
+
+            return longestSubString;
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Longest Substring Without Duplicates/LongestSubstringTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Longest_Substring_Without_Duplicates.Tests
+{
+    [TestClass()]
+    public class LongestSubstringTests
+    {
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest()
+        {
+            LongestSubstring.LengthOfLongestSubstring("zxyzxyz").Should().Be(3);
+        }
+
+        [TestMethod()]
+        public void LengthOfLongestSubstringxxxxxTest()
+        {
+            LongestSubstring.LengthOfLongestSubstring("xxxx").Should().Be(1);
+        }
+
+        [TestMethod()]
+        public void LengthOfLongestSubstring123456789Test()
+        {
+            LongestSubstring.LengthOfLongestSubstring("123456789").Should().Be(9);
+        }
+
+        [TestMethod()]
+        public void LengthOfLongestSubstringabcabcbbTest()
+        {
+            LongestSubstring.LengthOfLongestSubstring("abcabcbb").Should().Be(3);
+        }
+
+    }
+}
