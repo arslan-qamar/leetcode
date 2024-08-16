ab4d46df420684d766887e3b6ebab7a8492220ec
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-16 12:46:38 +1000

    13/150 done

---
+++ LeetCodeSolutions/NeetCode/Max Water Container/MaxWaterContainerCalculator.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCodeSolutions.NeetCode.Max_Water_Container
+{
+    public class MaxWaterContainerCalculator
+    {
+
+        public static int MaxArea(int[] heights)
+        {
+            if (heights == null || heights.Length < 1)
+            {
+                return 0;
+            }
+
+            int maxArea = 0;
+
+            int l = 0;
+            int r = heights.Length - 1;
+
+            while (l < r && l < heights.Length - 1 && r >= 0)
+            {
+                int minHeight = Math.Min(heights[l], heights[r]);
+
+                int area = minHeight * (r - l);
+
+                if (area > maxArea)
+                {
+                    maxArea = area;
+                }
+
+                if (heights[l] > heights[r])
+                {
+                    r--;
+                    continue;
+                }
+
+                if (heights[r] > heights[l])
+                {
+                    l++;
+                    continue;
+                }
+
+                if (heights[l] == heights[r])
+                {
+                    l++;
+                    continue;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Max Water Container/MaxWaterContainerCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Max_Water_Container.Tests
+{
+    [TestClass()]
+    public class MaxWaterContainerCalculatorTests
+    {
+        [TestMethod()]
+        public void MaxAreaTest()
+        {
+            MaxWaterContainerCalculator.MaxArea(new int[] { 1, 7, 2, 5, 4, 7, 3, 6 }).Should().Be(36);
+            MaxWaterContainerCalculator.MaxArea(new int[] { 2, 2, 2 }).Should().Be(4);
+            MaxWaterContainerCalculator.MaxArea(new int[] { }).Should().Be(0);
+
+        }
+
+        [TestMethod()]
+        public void SpecialCase()
+        {
+            MaxWaterContainerCalculator.MaxArea(new int[] { 1, 7, 2, 5, 12, 3, 500, 500, 7, 8, 4, 7, 3, 6 }).Should().Be(500);
+        }
+
+    }
+}
