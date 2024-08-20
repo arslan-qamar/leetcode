78cc16abb84d5df4db56a54a14dfe7905f5681cf
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-20 17:34:59 +1000

    21/150 done

---
+++ LeetCodeSolutions/NeetCode/Valid Parentheses/ValidParentheses.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeSolutions.NeetCode.Valid_Parentheses
+{
+    public class ValidParentheses
+    {
+        public static bool IsValid(string s)
+        {
+            if (s.Length < 1)
+            {
+                return false;
+            }
+
+            Stack<char> validationStack = new Stack<char>();
+
+            Dictionary<char, char> pairs = new Dictionary<char, char>()
+            {
+                [')'] = '(',
+                ['}'] = '{',
+                [']'] = '[',
+
+            };
+
+            foreach (char c in s)
+            {
+
+                if (c == '{' || c == '(' || c == '[')
+                {
+                    validationStack.Push(c);
+                }
+
+                if (pairs.ContainsKey(c))
+                {
+                    if (validationStack.Count == 0 || validationStack.Pop() != pairs[c])
+                        return false;
+                }
+            }
+
+            return validationStack.Count == 0;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Valid Parentheses/ValidParenthesesTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Valid_Parentheses.Tests
+{
+    [TestClass()]
+    public class ValidParenthesesTests
+    {
+        [TestMethod()]
+        public void IsValidTest()
+        {
+            ValidParentheses.IsValid("[]").Should().BeTrue();
+        }
+
+        [TestMethod()]
+        public void IsValidTest2()
+        {
+            ValidParentheses.IsValid("([{}])").Should().BeTrue();
+        }
+
+        [TestMethod()]
+        public void IsInValidTest()
+        {
+            ValidParentheses.IsValid("[(])").Should().BeFalse();
+        }
+
+
+
+        [TestMethod()]
+        public void IsInValidTestSpecial()
+        {
+            ValidParentheses.IsValid("]").Should().BeFalse();
+        }
+    }
+}
