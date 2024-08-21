2e1918b759fca2c538a7820fcb66671c746bbdd6
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-21 18:22:11 +1000

    25/150 done

---
+++ LeetCodeSolutions/NeetCode/Stacks/Daily Temperatures/DailyTemperaturesFinder.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutions.NeetCode.Stacks.Daily_Temperatures
+{
+    public class DailyTemperaturesFinder
+    {
+        public static int[] DailyTemperatures(int[] temperatures)
+        {
+            int[] result = new int[temperatures.Length];
+
+            LinkedList<int> monotonicStack = new LinkedList<int>();
+
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                while (monotonicStack.Count > 0 && temperatures[monotonicStack.Last.Value] < temperatures[i])
+                {
+                    result[monotonicStack.Last.Value] = i - monotonicStack.Last.Value;
+                    monotonicStack.RemoveLast();
+                }
+
+                monotonicStack.AddLast(i);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Stacks/Daily Temperatures/DailyTemperaturesFinderTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Stacks.Daily_Temperatures.Tests
+{
+    [TestClass()]
+    public class DailyTemperaturesFinderTests
+    {
+        [TestMethod()]
+        public void DailyTemperaturesTest()
+        {
+            DailyTemperaturesFinder.DailyTemperatures(new int[] { 30, 38, 30, 36, 35, 40, 28 }).Should().BeEquivalentTo(new int[] { 1, 4, 1, 2, 1, 0, 0 });
+            DailyTemperaturesFinder.DailyTemperatures(new int[] { 22, 21, 20 }).Should().BeEquivalentTo(new int[] { 0, 0, 0 });
+        }
+    }
+}
