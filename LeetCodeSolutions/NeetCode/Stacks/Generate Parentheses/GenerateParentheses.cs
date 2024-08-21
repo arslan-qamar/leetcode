1a78cb3ca72a73cf6372ec0c24df2d89fa7c7817
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-21 17:19:04 +1000

    23/150 done , refactored in folders

---
+++ LeetCodeSolutions/NeetCode/Stacks/Generate Parentheses/GenerateParentheses.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCodeSolutions.NeetCode.Stacks.Generate_Parentheses
+{
+    public class GenerateParentheses
+    {
+        public static List<string> GenerateParenthesis(int n)
+        {
+
+            List<string> result = new List<string>();
+
+            if (n < 1 || n > 7)
+            {
+                return result;
+            }
+
+            // if openC == closeC == n, return result
+            // if openC < n , append (
+            // if closeC < openC, append )
+
+            Stack<char> stack = new Stack<char>();
+
+            void backTrack(int openC, int closeC)
+            {
+                if (openC == closeC && closeC == n)
+                {
+                    result.Add(string.Join("", stack.Reverse()));
+                    return;
+                }
+
+                if (openC < n)
+                {
+                    stack.Push('(');
+                    backTrack(openC + 1, closeC);
+                    stack.Pop();
+                }
+
+                if (closeC < openC)
+                {
+                    stack.Push(')');
+                    backTrack(openC, closeC + 1);
+                    stack.Pop();
+                }
+
+            }
+
+            backTrack(0, 0);
+
+            return result;
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Stacks/Generate Parentheses/GenerateParenthesesTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Stacks.Generate_Parentheses.Tests
+{
+    [TestClass()]
+    public class GenerateParenthesesTests
+    {
+        [TestMethod()]
+        public void GenerateParenthesisTest()
+        {
+            GenerateParentheses.GenerateParenthesis(1).Should().BeEquivalentTo(new string[] { "()" });
+            GenerateParentheses.GenerateParenthesis(3).Should().BeEquivalentTo(new string[] { "((()))", "(()())", "(())()", "()(())", "()()()" });
+        }
+    }
+}
