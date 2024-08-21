5ae9542105877b1f62eb313ace40df9f5182ebe3
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-21 16:03:20 +1000

    23/150 done

---
+++ LeetCodeSolutions/NeetCode/Reverse Polish Notation/ReversePolishNotation.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeSolutions.NeetCode.Reverse_Polish_Notation
+{
+    public class ReversePolishNotation
+    {
+        public static int EvalRPN(string[] tokens)
+        {
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                switch (tokens[i])
+                {
+                    case "+":
+                        stack.Push(Convert.ToString(int.Parse(stack.Pop()) + int.Parse(stack.Pop())));
+                        break;
+
+                    case "*":
+                        stack.Push(Convert.ToString(int.Parse(stack.Pop()) * int.Parse(stack.Pop())));
+                        break;
+
+                    case "-":
+                        int firstPop = int.Parse(stack.Pop());
+                        int secondPop = int.Parse(stack.Pop());
+                        stack.Push(Convert.ToString(secondPop - firstPop));
+                        break;
+
+                    case "/":
+                        firstPop = int.Parse(stack.Pop());
+                        secondPop = int.Parse(stack.Pop());
+                        stack.Push(Convert.ToString(secondPop / firstPop));
+                        break;
+
+                    default:
+                        stack.Push(tokens[i]);
+                        break;
+
+                }
+            }
+
+            if (stack.Count == 1)
+            {
+                return int.Parse(stack.Pop());
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Reverse Polish Notation/ReversePolishNotationTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Reverse_Polish_Notation.Tests
+{
+    [TestClass()]
+    public class ReversePolishNotationTests
+    {
+        [TestMethod()]
+        public void EvalRPNTest()
+        {
+            ReversePolishNotation.EvalRPN(new string[] { "1", "2", "+", "3", "*", "4", "-" }).Should().Be(5);
+            ReversePolishNotation.EvalRPN(new string[] { "1", "2", "+" }).Should().Be(3);
+            ReversePolishNotation.EvalRPN(new string[] { "1" }).Should().Be(1);
+            ReversePolishNotation.EvalRPN(new string[] { "1", "2" }).Should().Be(0);
+
+        }
+    }
+}
