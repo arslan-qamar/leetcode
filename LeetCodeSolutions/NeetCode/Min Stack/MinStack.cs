c976c9a66d560884ddf0424df2f3c6cd445b788b
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-21 11:38:20 +1000

    22/150 done

---
+++ LeetCodeSolutions/NeetCode/Min Stack/MinStack.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCodeSolutions.NeetCode.Min_Stack
+{
+    public class MinStack
+    {
+        private readonly LinkedList<Tuple<int, int>> stack;
+        private int currentMin = int.MaxValue;
+        public MinStack()
+        {
+            stack = new LinkedList<Tuple<int, int>>();
+        }
+
+        public void Push(int val)
+        {
+            if (val < currentMin)
+            {
+                currentMin = val;
+            }
+
+            stack.AddFirst(new LinkedListNode<Tuple<int, int>>(new Tuple<int, int>(val, currentMin)));
+        }
+
+        public void Pop()
+        {
+            if (stack.Count > 0)
+            {
+                stack.RemoveFirst();
+
+                if (stack.Count == 0)
+                {
+                    currentMin = int.MaxValue;
+                }
+                else if (stack.First.Value.Item2 > currentMin)
+                {
+                    currentMin = stack.First.Value.Item2;
+                }
+            }
+        }
+
+        public int Top()
+        {
+            if (stack.Count > 0)
+            {
+                return stack.First.Value.Item1;
+            }
+
+            return int.MinValue;
+        }
+
+        public int GetMin()
+        {
+            if (stack.Count > 0)
+            {
+                return stack.First.Value.Item2;
+            }
+
+            return currentMin;
+        }
+    }
+}
