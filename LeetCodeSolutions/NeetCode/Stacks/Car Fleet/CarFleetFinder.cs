bffc4ebd5f540d7b512cdf942dd39db22543a488
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-22 11:25:25 +1000

    26/150 done

---
+++ LeetCodeSolutions/NeetCode/Stacks/Car Fleet/CarFleetFinder.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeSolutions.NeetCode.Stacks.Car_Fleet
+{
+    public class CarFleetFinder
+    {
+        public static int CarFleet(int target, int[] position, int[] speed)
+        {
+            var combined = position.Zip(speed, (p, s) => new { Position = p, Speed = s }).ToList();
+            combined.Sort((c, y) => c.Position.CompareTo(y.Position));
+
+            position = combined.Select(x => x.Position).ToArray();
+            speed = combined.Select(x => x.Speed).ToArray();
+
+            LinkedList<double> minDeque = new LinkedList<double>();
+
+            for (int i = 0; i < position.Length; i++)
+            {
+
+                double hops = (double)(target - position[i]) / speed[i];
+
+                while (minDeque.Count > 0 && minDeque.Last.Value <= hops)
+                {
+                    minDeque.RemoveLast();
+                }
+
+                minDeque.AddLast(hops);
+            }
+
+            return minDeque.Count;
+
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Stacks/Car Fleet/CarFleetFinderTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Stacks.Car_Fleet.Tests
+{
+    [TestClass()]
+    public class CarFleetFinderTests
+    {
+        [TestMethod()]
+        public void CarFleetTest()
+        {
+            CarFleetFinder.CarFleet(10, new int[] { 1, 4 }, new int[] { 3, 2 }).Should().Be(1);
+        }
+
+        [TestMethod()]
+        public void CarFleetTest2()
+        {
+            CarFleetFinder.CarFleet(10, new int[] { 4, 1, 0, 7 }, new int[] { 2, 2, 1, 1 }).Should().Be(3);
+        }
+
+        [TestMethod()]
+        public void CarFleetTest3()
+        {
+            CarFleetFinder.CarFleet(12, new int[] { 10, 8, 0, 5, 3 }, new int[] { 2, 4, 1, 1, 3 }).Should().Be(3);
+        }
+
+        [TestMethod()]
+        public void CarFleetTest4()
+        {
+            CarFleetFinder.CarFleet(10, new int[] { 6, 8 }, new int[] { 3, 2 }).Should().Be(2);
+        }
+
+
+    }
+}
