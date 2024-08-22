32c21e4a4cb70f4b38bc60e3d76a417810f4ab9d
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-22 17:08:15 +1000

    Binary Search

---
+++ LeetCodeSolutions/NeetCode/Binary Search/BinarySearch.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeSolutions.NeetCode.Binary_Search
+{
+    public class BinarySearch
+    {
+        public static int Search(int[] nums, int target)
+        {
+            int result = -1;
+
+            int l = 0;
+            int r = nums.Length - 1;
+
+            while (l <= r)
+            {
+                int m = l + ((r - l) / 2);
+
+                if (nums[m] > target)
+                {
+                    r = m - 1;
+                }
+                else if (nums[m] < target)
+                {
+                    l = m + 1;
+                }
+                else
+                {
+                    return m;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Binary Search/BinarySearchTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Binary_Search.Tests
+{
+    [TestClass()]
+    public class BinarySearchTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            BinarySearch.Search(new int[] { -1, 0, 2, 4, 6, 8 }, 4).Should().Be(3);
+            BinarySearch.Search(new int[] { -1, 0, 2, 4, 6, 8 }, 3).Should().Be(-1);
+        }
+
+        [TestMethod()]
+        public void SearchTestSpecial()
+        {
+            BinarySearch.Search(new int[] { -1, 0, 3, 5, 9, 12 }, 9).Should().Be(4);
+        }
+    }
+}
