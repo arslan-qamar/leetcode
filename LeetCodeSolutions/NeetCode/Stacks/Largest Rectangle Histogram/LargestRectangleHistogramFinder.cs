ed3bfdc51e1b49ea53fc9153ac3b17b23b60d32d
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-22 14:32:54 +1000

    Largest Historgram 27/150

---
+++ LeetCodeSolutions/NeetCode/Stacks/Largest Rectangle Histogram/LargestRectangleHistogramFinder.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodeSolutions.NeetCode.Stacks.Largest_Rectangle_Histogram
+{
+    public class LargestRectangleHistogramFinder
+    {
+        public static int LargestRectangleArea(int[] heights)
+        {
+            int maxArea = 0;
+
+            if (heights == null || heights.Length < 1)
+            {
+                return maxArea;
+            }
+
+            LinkedList<(int Index, int Height)> heightBlocks = new LinkedList<(int, int)>();
+
+            int extendBack = 0;
+            for (int i = 0; i < heights.Length; i++)
+            {
+                extendBack = i;
+                while (heightBlocks.Count > 0 && heightBlocks.Last.Value.Height > heights[i])
+                {
+                    maxArea = Math.Max(maxArea, heightBlocks.Last.Value.Height * (i - heightBlocks.Last.Value.Index));
+
+                    extendBack = heightBlocks.Last.Value.Index;
+
+                    heightBlocks.RemoveLast();
+                }
+
+                heightBlocks.AddLast(new LinkedListNode<(int Index, int Height)>((extendBack, heights[i])));
+            }
+
+            foreach ((int Index, int Height) item in heightBlocks)
+            {
+                maxArea = Math.Max(maxArea, item.Height * (heights.Length - item.Index));
+            }
+
+            return maxArea;
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Stacks/Largest Rectangle Histogram/LargestRectangleHistogramFinderTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Stacks.Largest_Rectangle_Histogram.Tests
+{
+    [TestClass()]
+    public class LargestRectangleHistogramFinderTests
+    {
+        [TestMethod()]
+        public void LargestRectangleAreaTest()
+        {
+            LargestRectangleHistogramFinder.LargestRectangleArea(new int[] { 7, 1, 7, 2, 2, 4 }).Should().Be(8);
+        }
+
+        [TestMethod()]
+        public void LargestRectangleAreaTest2()
+        {
+            LargestRectangleHistogramFinder.LargestRectangleArea(new int[] { 1, 3, 7 }).Should().Be(7);
+        }
+
+    }
+}
