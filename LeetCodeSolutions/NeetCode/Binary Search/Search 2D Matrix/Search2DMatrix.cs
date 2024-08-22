cf685201914411ec16d6625040bfcc7207386574
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-23 09:27:30 +1000

    29/150 done

---
+++ LeetCodeSolutions/NeetCode/Binary Search/Search 2D Matrix/Search2DMatrix.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeSolutions.NeetCode.Binary_Search.Search_2D_Matrix
+{
+    public class Search2DMatrix
+    {
+        public static bool SearchMatrix(int[][] matrix, int target)
+        {
+            bool result = false;
+
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+
+            int totalBoxes = rows * cols;
+            int l = 0;
+            int r = totalBoxes - 1;
+
+            while (l <= r)
+            {
+                int m = l + ((r - l) / 2);
+                int mx = m / cols;
+                int my = m % cols;
+
+                if (matrix[mx][my] > target)
+                {
+                    r = m - 1;
+                }
+                else if (matrix[mx][my] < target)
+                {
+                    l = m + 1;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Binary Search/Search 2D Matrix/Search2DMatrixTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Binary_Search.Search_2D_Matrix.Tests
+{
+    [TestClass()]
+    public class Search2DMatrixTests
+    {
+        [TestMethod()]
+        public void SearchMatrixTest()
+        {
+            Search2DMatrix.SearchMatrix(new int[][]
+            {
+                new int [] {1,2,4,8 },
+                new int [] {10, 11, 12, 13 },
+                new int [] {14, 20, 30, 40 }
+            }, 10).Should().BeTrue();
+        }
+
+        [TestMethod()]
+        public void SearchMatrixTest2()
+        {
+            Search2DMatrix.SearchMatrix(new int[][]
+            {
+                new int [] {1,2,4,8 },
+                new int [] {10, 11, 12, 13 },
+                new int [] {14, 20, 30, 40 }
+            }, 15).Should().BeFalse();
+        }
+
+        [TestMethod()]
+        public void SearchMatrixTest3()
+        {
+            Search2DMatrix.SearchMatrix(new int[][]
+            {
+                new int [] {1,3,5,7 },
+                new int [] {10,11,16,20 },
+                new int [] { 23, 30, 34, 60 }
+            }, 3).Should().BeTrue();
+        }
+
+
+        [TestMethod()]
+        public void SearchMatrixTest4()
+        {
+            Search2DMatrix.SearchMatrix(new int[][]
+            {
+                new int [] {1},
+                new int [] {3}
+            }, 3).Should().BeTrue();
+        }
+
+        [TestMethod()]
+        public void SearchMatrixTest5()
+        {
+            Search2DMatrix.SearchMatrix(new int[][]
+            {
+                new int [] {1}
+            }, 1).Should().BeTrue();
+        }
+    }
+}
