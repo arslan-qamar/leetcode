6a0e266f168d6395e9b1fd03c995fd792991bd59
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-23 11:39:27 +1000

    31/150 done

---
+++ LeetCodeSolutions/NeetCode/Binary Search/Find Minimum Rotated Sorted Array/MinimumRotatedSortedArray.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCodeSolutions.NeetCode.Binary_Search.Find_Minimum_Rotated_Sorted_Array
+{
+    public class MinimumRotatedSortedArray
+    {
+        public static int FindMin(int[] nums)
+        {
+
+            if (nums == null || nums.Length < 1)
+            {
+                return 0;
+            }
+
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            int result = int.MaxValue;
+
+            int l = 0;
+            int r = nums.Length - 1;
+
+            while (l <= r)
+            {
+                if (nums[l] < nums[r])
+                {
+                    result = Math.Min(result, nums[l]);
+                    break;
+                }
+
+                int m = (l + r) / 2;
+
+                result = Math.Min(result, nums[m]);
+
+                if (nums[m] >= nums[l])
+                {
+                    l = m + 1;
+                }
+                else
+                {
+                    r = m - 1;
+                }
+
+            }
+
+
+            return result;
+        }
+
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Binary Search/Find Minimum Rotated Sorted Array/MinimumRotatedSortedArrayTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Binary_Search.Find_Minimum_Rotated_Sorted_Array.Tests
+{
+    [TestClass()]
+    public class MinimumRotatedSortedArrayTests
+    {
+        [TestMethod()]
+        public void FindMinTest()
+        {
+            MinimumRotatedSortedArray.FindMin(new int[] { 3, 4, 5, 6, 1, 2 }).Should().Be(1);
+        }
+
+        [TestMethod()]
+        public void FindMinTest2()
+        {
+            MinimumRotatedSortedArray.FindMin(new int[] { 3 }).Should().Be(3);
+        }
+
+
+        [TestMethod()]
+        public void FindMinTest3()
+        {
+            MinimumRotatedSortedArray.FindMin(new int[] { 4, 5, 0, 1, 2, 3 }).Should().Be(0);
+        }
+
+
+        [TestMethod()]
+        public void FindMinTest4()
+        {
+            MinimumRotatedSortedArray.FindMin(new int[] { 4, 5, 6, 7 }).Should().Be(4);
+        }
+
+        [TestMethod()]
+        public void FindMinTest5()
+        {
+            MinimumRotatedSortedArray.FindMin(new int[] { 1, 2 }).Should().Be(1);
+        }
+
+    }
+}
