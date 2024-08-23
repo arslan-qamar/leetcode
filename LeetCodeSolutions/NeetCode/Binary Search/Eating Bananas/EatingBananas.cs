a3817103f324135dd6aa20abe5dc62eb8acb74cc
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-23 10:34:08 +1000

    30/150 done

---
+++ LeetCodeSolutions/NeetCode/Binary Search/Eating Bananas/EatingBananas.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCodeSolutions.NeetCode.Binary_Search.Eating_Bananas
+{
+    public class EatingBananas
+    {
+
+        public static int MinEatingSpeed(int[] piles, int h)
+        {
+
+            if (piles == null || piles.Length < 1 || h == 0 || piles.Length > h)
+            {
+                return 0;
+            }
+
+            int l = 1;
+            int r = piles.Max();
+
+            int minK = int.MaxValue;
+
+            while (l <= r)
+            {
+                int k = l + ((r - l) / 2);
+
+                int currH = 0;
+                for (int i = 0; i < piles.Length; i++)
+                {
+                    currH = currH + (int)Math.Ceiling((double)piles[i] / k);
+                }
+
+                if (currH > h)
+                {
+                    l = k + 1;
+                }
+                else if (currH <= h)
+                {
+                    r = k - 1;
+                    minK = Math.Min(minK, k);
+                }
+            }
+
+            return minK;
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Binary Search/Eating Bananas/EatingBananasTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Binary_Search.Eating_Bananas.Tests
+{
+    [TestClass()]
+    public class EatingBananasTests
+    {
+        [TestMethod()]
+        public void MinEatingSpeedTest()
+        {
+            EatingBananas.MinEatingSpeed(new int[] { 1, 4, 3, 2 }, 9).Should().Be(2);
+        }
+
+        [TestMethod()]
+        public void MinEatingSpeedTest2()
+        {
+            EatingBananas.MinEatingSpeed(new int[] { 25, 10, 23, 4 }, 4).Should().Be(25);
+        }
+    }
+}
