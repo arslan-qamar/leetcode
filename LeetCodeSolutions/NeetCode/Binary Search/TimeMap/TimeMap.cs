de4489ef1de32370f747ce7c2df323fde8a28115
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-23 16:08:02 +1000

    33/150 done

---
+++ LeetCodeSolutions/NeetCode/Binary Search/TimeMap/TimeMap.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeSolutions.NeetCode.Binary_Search.TimeMap
+{
+    public class TimeMap
+    {
+        private readonly Dictionary<string, List<(int TimeStamp, string Value)>> keyValuePairs;
+
+        public TimeMap()
+        {
+            keyValuePairs = new Dictionary<string, List<(int TimeStamp, string Value)>>();
+        }
+
+        public void Set(string key, string value, int timestamp)
+        {
+            keyValuePairs[key] = keyValuePairs.TryGetValue(key, out List<(int TimeStamp, string Value)> storedValues) ? storedValues : new List<(int TimeStamp, string Value)>();
+            keyValuePairs[key].Add((timestamp, value));
+        }
+
+        public string Get(string key, int timestamp)
+        {
+            string result = "";
+
+            if (keyValuePairs.ContainsKey(key))
+            {
+
+                List<(int TimeStamp, string Value)> values = keyValuePairs[key];
+                int left = 0, right = values.Count - 1;
+
+
+                while (left <= right)
+                {
+                    int mid = left + (right - left) / 2;
+                    if (values[mid].TimeStamp <= timestamp)
+                    {
+                        result = values[mid].Item2;
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+            }
+
+            return result;
+
+
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Binary Search/TimeMap/TimeMapTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Binary_Search.TimeMap.Tests
+{
+    [TestClass()]
+    public class TimeMapTests
+    {
+        [TestMethod()]
+        public void TimeMapTest()
+        {
+            TimeMap timeMap = new TimeMap();
+            timeMap.Set("alice", "happy", 1);  // store the key "alice" and value "happy" along with timestamp = 1.
+            timeMap.Get("alice", 1).Should().Be("happy");           // return "happy"
+            timeMap.Get("alice", 2).Should().Be("happy");           // return "happy", there is no value stored for timestamp 2, thus we return the value at timestamp 1.
+            timeMap.Set("alice", "sad", 3);    // store the key "alice" and value "sad" along with timestamp = 3.
+            timeMap.Get("alice", 3).Should().Be("sad");
+        }
+
+        [TestMethod()]
+        public void TimeMapTest2()
+        {
+            TimeMap timeMap = new TimeMap();
+            timeMap.Set("alice", "happy", 1);  // store the key "alice" and value "happy" along with timestamp = 1.
+            timeMap.Get("alice", 1).Should().Be("happy");           // return "happy"
+            timeMap.Get("alice", 2).Should().Be("happy");           // return "happy", there is no value stored for timestamp 2, thus we return the value at timestamp 1.
+            timeMap.Set("alice", "sad", 3);    // store the key "alice" and value "sad" along with timestamp = 3.
+            timeMap.Get("alice", 3).Should().Be("sad");
+        }
+    }
+}
