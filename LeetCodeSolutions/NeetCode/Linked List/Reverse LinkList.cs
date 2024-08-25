5d1b3daf3e12fea142bfe4ef4a86a82fc826a93e
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-25 11:58:50 +1000

    35/150

---
+++ LeetCodeSolutions/NeetCode/Linked List/Reverse LinkList.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeSolutions.NeetCode.Linked_List
+{
+    public class ListNode
+    {
+        public readonly int val;
+        public ListNode next;
+
+        public ListNode(int val, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Reverse_LinkList
+    {
+        public static ListNode ReverseList(ListNode head)
+        {
+            ListNode previous = null;
+            ListNode current = head;
+            while (current != null)
+            {
+                ListNode temp = current.next;
+                current.next = previous;
+                previous = current;
+                current = temp;
+            }
+
+            return previous;
+        }
+
+    }
+
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Linked List/Reverse_LinkListTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Linked_List.Tests
+{
+    [TestClass()]
+    public class Reverse_LinkListTests
+    {
+        [TestMethod()]
+        public void ReverseListTest()
+        {
+            Reverse_LinkList.ReverseList(null).Should().Be(null);
+        }
+
+        [TestMethod()]
+        public void ReverseListTest1()
+        {
+            Reverse_LinkList.ReverseList(new ListNode(1, null)).Should().BeEquivalentTo(new ListNode(1, null));
+        }
+
+        [TestMethod()]
+        public void ReverseListTest2()
+        {
+            Reverse_LinkList.ReverseList(new ListNode(1, new ListNode(2, null)))
+                .Should().BeEquivalentTo(new ListNode(2, new ListNode(1, null)), options => options.WithStrictOrdering());
+        }
+
+        [TestMethod()]
+        public void ReverseListTest3()
+        {
+            Reverse_LinkList.ReverseList(new ListNode(1, new ListNode(2, new ListNode(3, null))))
+                .Should().BeEquivalentTo(new ListNode(3, new ListNode(2, new ListNode(1, null))), options => options.WithStrictOrdering());
+        }
+
+    }
+}
