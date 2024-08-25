ccd7ae5d5c300152d115594fe81b388496721f08
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-25 17:04:26 +1000

    37/150 done

---
+++ LeetCodeSolutions/NeetCode/Linked List/Reorder List/ReorderListHandler.cs
@@ -0,0 +1,61 @@
+﻿namespace LeetCodeSolutions.NeetCode.Linked_List.Reorder_List
+{
+    public class ListNode
+    {
+        public readonly int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class ReorderListHandler
+    {
+        public static void ReorderList(ListNode head)
+        {
+
+            ListNode slowPtr = head;
+            ListNode fastPtr = head.next;
+
+
+            while (fastPtr?.next != null)
+            {
+                slowPtr = slowPtr.next;
+                fastPtr = fastPtr.next.next;
+
+            }
+
+            // Reverse the Right half of the list            
+
+            ListNode curr = slowPtr.next;
+            ListNode previous = slowPtr.next = null;
+
+            while (curr != null)
+            {
+                ListNode temp = curr.next;
+                curr.next = previous;
+                previous = curr;
+                curr = temp;
+            }
+
+            ListNode leftListStart = head;
+            ListNode rightListStart = previous;
+
+            while (rightListStart != null)
+            {
+
+                ListNode ltemp = leftListStart.next;
+                ListNode rtemp = rightListStart.next;
+
+                leftListStart.next = rightListStart;
+                rightListStart.next = ltemp;
+
+                leftListStart = ltemp;
+                rightListStart = rtemp;
+            }
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Linked List/Reorder List/ReorderListHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace LeetCodeSolutions.NeetCode.Linked_List.Reorder_List.Tests
+{
+    [TestClass()]
+    public class ReorderListHandlerTests
+    {
+        [TestMethod()]
+        public void ReorderListTest()
+        {
+            ListNode listNode = new ListNode(2, new ListNode(4, new ListNode(6, new ListNode(8, null))));
+            ReorderListHandler.ReorderList(listNode);
+
+            listNode.ShouldBeEquivalentTo(new ListNode(2, new ListNode(8, new ListNode(4, new ListNode(6, null)))));
+        }
+
+        [TestMethod()]
+        public void ReorderListTest2()
+        {
+            ListNode listNode = new ListNode(2, new ListNode(4, new ListNode(6, null)));
+            ReorderListHandler.ReorderList(listNode);
+
+            listNode.ShouldBeEquivalentTo(new ListNode(2, new ListNode(6, new ListNode(4, null))));
+        }
+
+
+        [TestMethod()]
+        public void ReorderListTest3()
+        {
+            ListNode listNode = new ListNode(2, new ListNode(4, new ListNode(6, new ListNode(8, new ListNode(10, new ListNode(12, null))))));
+            ReorderListHandler.ReorderList(listNode);
+
+            listNode.ShouldBeEquivalentTo(new ListNode(2, new ListNode(12, new ListNode(4, new ListNode(10, new ListNode(6, new ListNode(8, null)))))));
+        }
+    }
+}
