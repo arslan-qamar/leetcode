de4935327fa018205903a619620798a16d3deee7
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-25 18:13:27 +1000

    Remove node from end of the list 38/150

---
+++ LeetCodeSolutions/NeetCode/Linked List/Remove Node From End/RemoveNodeFromEnd.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodeSolutions.NeetCode.Linked_List.Remove_Node_From_End
+{
+    public class ListNode
+    {
+        public readonly int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class RemoveNodeFromEnd
+    {
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode dummy = new ListNode(0, head);
+            ListNode left = dummy;
+            ListNode right = head;
+
+
+            while (n > 0)
+            {
+                right = right.next;
+                n--;
+            }
+
+
+            while (right != null)
+            {
+                left = left.next;
+                right = right.next;
+            }
+
+            left.next = left.next.next;
+            return dummy.next;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Linked List/Remove Node From End/RemoveNodeFromEndTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Linked_List.Remove_Node_From_End.Tests
+{
+    [TestClass()]
+    public class RemoveNodeFromEndTests
+    {
+        [TestMethod()]
+        public void RemoveNthFromEndTest()
+        {
+            RemoveNodeFromEnd.RemoveNthFromEnd(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, null)))), 2)
+                .Should().BeEquivalentTo(new ListNode(1, new ListNode(2, new ListNode(4, null))));
+        }
+
+        [TestMethod()]
+        public void RemoveNthFromEndTest2()
+        {
+            RemoveNodeFromEnd.RemoveNthFromEnd(new ListNode(5, null), 1)
+                .Should().BeNull();
+        }
+
+    }
+}
