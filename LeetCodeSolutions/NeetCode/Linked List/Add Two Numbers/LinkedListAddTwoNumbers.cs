b2a2b5634983c8d4d5d93b1c66cafeffc06e7bbf
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-26 11:19:29 +1000

    Add Two Numbers.

---
+++ LeetCodeSolutions/NeetCode/Linked List/Add Two Numbers/LinkedListAddTwoNumbers.cs
@@ -0,0 +1,81 @@
+﻿namespace LeetCodeSolutions.NeetCode.Linked_List.Add_Two_Numbers
+{
+    public class ListNode
+    {
+        public readonly int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public class LinkedListAddTwoNumbers
+    {
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode currL1 = l1;
+            ListNode currL2 = l2;
+
+            ListNode dummy = new ListNode(0, null);
+            ListNode result = dummy;
+
+            int carry = 0;
+            int sum;
+            while (currL1 != null && currL2 != null)
+            {
+
+                sum = carry + currL1.val + currL2.val;
+
+                carry = sum / 10;
+
+                sum = sum % 10;
+
+                result.next = new ListNode(sum, null);
+                result = result.next;
+
+                currL1 = currL1.next;
+                currL2 = currL2.next;
+            }
+
+            while (currL1 != null)
+            {
+                sum = carry + currL1.val;
+
+                carry = sum / 10;
+
+                sum = sum % 10;
+
+                result.next = new ListNode(sum, null);
+                result = result.next;
+
+                currL1 = currL1.next;
+
+            }
+
+            while (currL2 != null)
+            {
+                sum = carry + currL2.val;
+
+                carry = sum / 10;
+
+                sum = sum % 10;
+
+                result.next = new ListNode(sum, null);
+                result = result.next;
+
+                currL2 = currL2.next;
+            }
+
+            if (carry > 0)
+            {
+                result.next = new ListNode(carry, null);
+            }
+
+            return dummy.next;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Linked List/Add Two Numbers/LinkedListAddTwoNumbersTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Linked_List.Add_Two_Numbers.Tests
+{
+    [TestClass()]
+    public class LinkedListAddTwoNumbersTests
+    {
+        [TestMethod()]
+        public void AddTwoNumbersTest()
+        {
+
+            ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, null)));
+            ListNode l2 = new ListNode(4, new ListNode(5, new ListNode(6, null)));
+
+            LinkedListAddTwoNumbers.AddTwoNumbers(l1, l2).Should().BeEquivalentTo(
+                new ListNode(5, new ListNode(7, new ListNode(9, null)))
+                );
+        }
+
+        [TestMethod()]
+        public void AddTwoNumbersTest2()
+        {
+
+            ListNode l1 = new ListNode(9, null);
+            ListNode l2 = new ListNode(9, null);
+
+            LinkedListAddTwoNumbers.AddTwoNumbers(l1, l2).Should().BeEquivalentTo(
+                new ListNode(8, new ListNode(1, null))
+                );
+        }
+    }
+}
