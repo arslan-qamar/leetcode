0321416133ca114fc672b55a0486b3e2e388b8f7
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-26 11:32:49 +1000

    41/150

---
+++ LeetCodeSolutions/NeetCode/Linked List/Linked List Cycle Detect/CycleDetect.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeSolutions.NeetCode.Linked_List.Linked_List_Cycle_Detect
+{
+    public class ListNode
+    {
+        public readonly int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+    public class CycleDetect
+    {
+        public static bool HasCycle(ListNode head)
+        {
+            ListNode currSlow = head;
+            ListNode currFast = head.next;
+            while (currSlow != null && currFast != null && currSlow != currFast)
+            {
+                currSlow = currSlow.next;
+                currFast = currFast.next?.next;
+            }
+
+            return currFast == currSlow;
+        }
+    }
+}
