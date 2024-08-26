5a88ab7079e129151d9d4ea031991febfaac5c62
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-26 15:45:28 +1000

    43/150 Done

---
+++ LeetCodeSolutions/NeetCode/Linked List/LRU Cache/LRUCache.cs
@@ -0,0 +1,87 @@
+﻿namespace LeetCodeSolutions.NeetCode.Linked_List.LRU_Cache
+{
+    public class LRUCache
+    {
+        public class ListNode
+        {
+            public readonly int key;
+            public readonly int val;
+            public ListNode next;
+            public ListNode previous;
+
+            public ListNode(int key = 0, int val = 0, ListNode next = null, ListNode previous = null)
+            {
+                this.key = key;
+                this.val = val;
+                this.next = next;
+                this.previous = previous;
+            }
+        }
+
+
+        private readonly int capacity;
+        private readonly Dictionary<int, ListNode> cache = new Dictionary<int, ListNode>();
+
+        private readonly ListNode head;
+        private readonly ListNode tail;
+
+        public LRUCache(int capacity)
+        {
+            this.capacity = capacity;
+
+            this.head = new ListNode(int.MinValue);
+            this.tail = new ListNode(int.MaxValue);
+
+            this.head.next = this.tail;
+            this.tail.previous = this.head;
+        }
+
+        public int Get(int key)
+        {
+            cache.TryGetValue(key, out ListNode? val);
+
+            if (cache.ContainsKey(key))
+            {
+                RemoveNode(val);
+                AddNode(val);
+            }
+
+            return val?.val ?? -1;
+        }
+
+        private void RemoveNode(ListNode? val)
+        {
+            val.previous.next = val.next;
+            val.next.previous = val.previous;
+        }
+
+        private void AddNode(ListNode? val)
+        {
+            val.next = tail;
+            val.previous = tail.previous;
+
+            tail.previous.next = val;
+            tail.previous = val;
+        }
+
+        public void Put(int key, int value)
+        {
+            ListNode newValue = new ListNode(key, value);
+
+            if (cache.ContainsKey(key))
+            {
+                cache.TryGetValue(key, out ListNode? val);
+                RemoveNode(val);
+            }
+
+            AddNode(newValue);
+            cache[key] = newValue;
+
+            if (cache.Count > capacity)
+            {
+                cache.Remove(head.next.key);
+                RemoveNode(head.next);
+            }
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Linked List/LRU Cache/LRUCacheTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace LeetCodeSolutions.NeetCode.Linked_List.LRU_Cache.Tests
+{
+    [TestClass()]
+    public class LRUCacheTests
+    {
+        [TestMethod()]
+        public void LRUCacheTest()
+        {
+            LRUCache lRUCache = new LRUCache(1);
+
+            lRUCache.Put(1, 1);
+            lRUCache.Put(2, 1);
+            lRUCache.Put(3, 1);
+
+            lRUCache.Get(1).ShouldBe(-1);
+            lRUCache.Get(2).ShouldBe(-1);
+            lRUCache.Get(3).ShouldBe(1);
+
+        }
+
+        [TestMethod()]
+        public void LRUCacheTest2()
+        {
+            LRUCache lRUCache = new LRUCache(2);
+
+            lRUCache.Put(1, 1);
+            lRUCache.Put(1, 1);
+            lRUCache.Put(2, 1);
+
+            lRUCache.Get(1).ShouldBe(1);
+            lRUCache.Get(2).ShouldBe(1);
+
+            lRUCache.Put(3, 1);
+
+            lRUCache.Get(3).ShouldBe(1);
+            lRUCache.Get(1).ShouldBe(-1);
+
+        }
+
+
+    }
+}
