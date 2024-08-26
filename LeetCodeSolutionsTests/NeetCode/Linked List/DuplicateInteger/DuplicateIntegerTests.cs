473cf6d33ceec010dcd246c55d0540798bb62502
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-26 12:15:53 +1000

    42/150

---
+++ LeetCodeSolutionsTests/NeetCode/Linked List/DuplicateInteger/DuplicateIntegerTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Linked_List.DuplicateInteger.Tests
+{
+    [TestClass()]
+    public class DuplicateIntegerTests
+    {
+        [TestMethod()]
+        public void FindDuplicateTest()
+        {
+            DuplicateInteger.FindDuplicate(new int[] { 1, 2, 3, 2, 2 }).Should().Be(2);
+        }
+
+        [TestMethod()]
+        public void FindDuplicateTest2()
+        {
+            DuplicateInteger.FindDuplicate(new int[] { 1, 2, 3, 4, 4 }).Should().Be(4);
+        }
+    }
+}
