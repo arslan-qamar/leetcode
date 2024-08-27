764ded6bb98a5b172583f8cf9d8c5ba31096942e
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-28 09:41:06 +1000

    47/150 done

---
+++ LeetCodeSolutions/NeetCode/Trees/MaxDepth/BinaryTreeMaxDepth.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeSolutions.NeetCode.Trees.InvertBST;
+
+namespace LeetCodeSolutions.NeetCode.Trees.MaxDepth
+{
+    public class BinaryTreeMaxDepth
+    {
+        private static int MDepth = 0;
+        public static int MaxDepth(TreeNode root)
+        {
+            MaxDepth(root, 0);
+            return MDepth;
+        }
+
+        public static void MaxDepth(TreeNode root, int depth)
+        {
+
+            if (root == null)
+            {
+                MDepth = Math.Max(MDepth, depth);
+                return;
+            }
+
+            depth++;
+
+            MaxDepth(root.left, depth);
+            MaxDepth(root.right, depth);
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Trees/InvertBST/InvertBinarySearchTreeTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Trees.InvertBST.Tests
+{
+    [TestClass()]
+    public class InvertBinarySearchTreeTests
+    {
+        [TestMethod()]
+        public void InvertTreeTest()
+        {
+            InvertBinarySearchTree.InvertTree(
+                new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6), new TreeNode(7)))
+                ).Should().BeEquivalentTo(
+                new TreeNode(1, new TreeNode(3, new TreeNode(7), new TreeNode(6)), new TreeNode(2, new TreeNode(5), new TreeNode(4)))
+                );
+
+        }
+    }
+}
