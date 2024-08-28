41506446e86c57a0e5ae231246c33f4e9cfae0d6
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-28 16:20:21 +1000

    Balanced BST Checker 49/150 done

---
+++ LeetCodeSolutions/NeetCode/Trees/Balanced BST Checker/BSTBalanceChecker.cs
@@ -0,0 +1,45 @@
+﻿namespace Balanced_BST_Checker
+{
+    public class BSTBalanceChecker
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public static bool IsBalanced(TreeNode root)
+        {
+            bool result =true;
+
+            int CheckHeights(TreeNode node)
+            {
+                if(node == null || result == false)
+                {
+                    return 0;
+                }
+
+                int left = CheckHeights(node.left);
+                int right = CheckHeights(node.right);
+
+                if(left - right > 1 || left -  right < -1)
+                {
+                    result = false;
+                }
+
+                return 1 + Math.Max(left, right);
+            }
+
+            CheckHeights(root);
+
+            return result;            
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Trees/Balanced BST Checker/BSTBalanceCheckerTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Balanced_BST_Checker.BSTBalanceChecker;
+
+namespace Balanced_BST_Checker.Tests
+{
+    [TestClass()]
+    public class BSTBalanceCheckerTests
+    {
+        [TestMethod()]
+        public void IsBalancedTest()
+        {
+            BSTBalanceChecker.IsBalanced(
+                new TreeNode(1, new TreeNode(2), new TreeNode(3, new TreeNode(4)))
+                ).Should().BeTrue();
+        }
+    }
+}
