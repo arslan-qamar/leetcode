0b0bd44e5988c32827ea04899ab8529705f00429
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-28 16:37:25 +1000

    50/150 done

---
+++ LeetCodeSolutions/NeetCode/Trees/Equal BST/EqualBST.cs
@@ -0,0 +1,42 @@
+﻿namespace Equal_BST
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+    public class EqualBST
+    {
+        static bool result = true;
+        public static bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (result == false)
+                return false;
+
+            if (p == q)
+            {
+                return true;
+            }
+
+            if ((p == null && q != null) || (p != null && q == null) || p.val != q.val)
+            {
+                result = false;
+                return result;
+            }
+            
+            IsSameTree(p.left, q.left);
+            IsSameTree(p.right, q.right);
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Trees/Equal BST/EqualBSTTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equal_BST.Tests
+{
+    [TestClass()]
+    public class EqualBSTTests
+    {
+        [TestMethod()]
+        public void IsSameTreeTest()
+        {
+            EqualBST.IsSameTree(
+                new TreeNode(1, new TreeNode(2), new TreeNode(3)),
+                new TreeNode(1, new TreeNode(2), new TreeNode(3))
+                ).Should().BeTrue();
+
+        }
+
+        [TestMethod()]
+        public void IsSameTreeTest2()
+        {
+            EqualBST.IsSameTree(
+                new TreeNode(4, new TreeNode(7)),
+                new TreeNode(4, null, new TreeNode(7))
+                ).Should().BeFalse();
+
+
+        }
+    }
+}
