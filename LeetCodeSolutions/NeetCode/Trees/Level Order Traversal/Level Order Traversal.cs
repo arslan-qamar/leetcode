71cf07e11c5289cb22e336bc39c42fdf77c5c947
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-29 12:14:42 +1000

    BFS traversal

---
+++ LeetCodeSolutions/NeetCode/Trees/Level Order Traversal/Level Order Traversal.cs
@@ -0,0 +1,56 @@
+﻿namespace Level_Order_Traversal
+{
+    public class Level_Order_Traversal
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public List<List<int>> LevelOrder(TreeNode root)
+        {
+            if(root == null)
+            {
+                return new List<List<int>>();
+            }
+            List<List<int>> result = new List<List<int>>();
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+
+                List<int> subList = new List<int>();
+                int count = queue.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    TreeNode node = queue.Dequeue();
+                    subList.Add(node.val);
+
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                result.Add(subList);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Trees/Level Order Traversal/Level_Order_TraversalTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Level_Order_Traversal.Level_Order_Traversal;
+
+namespace Level_Order_Traversal.Tests
+{
+    [TestClass()]
+    public class Level_Order_TraversalTests
+    {
+        [TestMethod()]
+        public void LevelOrderTest()
+        {
+            Level_Order_Traversal levelOrderTraversal = new Level_Order_Traversal();
+
+
+            levelOrderTraversal.LevelOrder(
+
+                new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(6), new TreeNode(7)))
+                ).Should().BeEquivalentTo(
+                new List<List<int>>()
+                {
+                    new List<int>() {1 },
+                    new List<int>() { 2,3},
+                    new List<int>() { 4,5,6,7}
+                }
+
+                );
+        }
+    }
+}
