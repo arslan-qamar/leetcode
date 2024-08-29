32b4cfe87ae5526fb9837c75e131203be045a75c
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-29 11:27:43 +1000

    LCA 52/150

---
+++ LeetCodeSolutions/NeetCode/Trees/Lowest Common Ancestor of BST/LowestCommonAncestorFinder.cs
@@ -0,0 +1,64 @@
+﻿namespace Lowest_Common_Ancestor_of_BST
+{
+    public class LowestCommonAncestorFinder
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+
+            TreeNode curr = root;
+
+            while (true)
+            {
+                if (p.val > curr.val && q.val > curr.val)
+                {
+                    curr = curr.right;
+                }
+                else if (p.val < curr.val && q.val < curr.val)
+                {
+                    curr = curr.left;
+                }
+                else
+                {
+                    return curr;
+                }
+            }           
+
+        }
+
+        private (int height, bool found) treeContains(TreeNode root, TreeNode target)
+        {
+            if (root == null)
+            {
+                return (-1, false);
+            }
+
+            if(root.val == target.val)
+            {
+                return (0, true);
+            }
+
+            (int height, bool found) leftedges = treeContains(root.left, target);
+            (int height, bool found) rightedges = treeContains(root.right, target);
+
+            if (leftedges.found || rightedges.found)
+            {
+                return (1 + Math.Max(leftedges.height, rightedges.height), true);
+            }
+
+            return (int.MinValue, false);
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Trees/Lowest Common Ancestor of BST/LowestCommonAncestorFinderTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Lowest_Common_Ancestor_of_BST.LowestCommonAncestorFinder;
+
+namespace Lowest_Common_Ancestor_of_BST.Tests
+{
+    [TestClass()]
+    public class LowestCommonAncestorFinderTests
+    {
+        [TestMethod()]
+        public void LowestCommonAncestorTest()
+        {
+            var lcaFinder = new LowestCommonAncestorFinder();
+
+
+            lcaFinder.LowestCommonAncestor(
+                new TreeNode(5, new TreeNode(3, new TreeNode(1, null, new TreeNode(2)), new TreeNode(4)), new TreeNode(8, new TreeNode(7), new TreeNode(9))),
+                new TreeNode(3),
+                new TreeNode(4)
+                ).Should().BeEquivalentTo(new TreeNode(3, new TreeNode(1, null, new TreeNode(2)), new TreeNode(4)));
+        }
+
+        [TestMethod()]
+        public void LowestCommonAncestorTest2()
+        {
+            var lcaFinder = new LowestCommonAncestorFinder();
+
+
+            lcaFinder.LowestCommonAncestor(
+                new TreeNode(5, new TreeNode(3, new TreeNode(1, null, new TreeNode(2)), new TreeNode(4)), new TreeNode(8, new TreeNode(7), new TreeNode(9))),
+                new TreeNode(3),
+                new TreeNode(8)
+                ).Should().BeEquivalentTo(new TreeNode(5, new TreeNode(3, new TreeNode(1, null, new TreeNode(2)), new TreeNode(4)), new TreeNode(8, new TreeNode(7), new TreeNode(9))));
+        }
+
+    }
+}
