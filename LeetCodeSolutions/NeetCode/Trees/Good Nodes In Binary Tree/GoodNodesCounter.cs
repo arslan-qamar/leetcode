964dbd06ffe6cb6bf63df9ce50edfa0a36b32a66
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-31 06:14:32 +1000

    55/150 done

--- LeetCodeSolutions/NeetCode/Trees/Good Nodes In Binary Tree/GoodNodesCounter.cs
+++ LeetCodeSolutions/NeetCode/Trees/Good Nodes In Binary Tree/GoodNodesCounter.cs
@@ -4,9 +4,32 @@ namespace Good_Nodes_In_Binary_Tree
 {
     public class GoodNodesCounter
     {
+        
         public static int GoodNodes(TreeNode root)
+        {        
+            return FindGoodNodes(root, root.val);
+        }
+
+        private static int FindGoodNodes(TreeNode? node, int maxSeen)
         {
-            return 0;
+
+            if (node == null)
+            {
+                return 0;
+            }
+
+            int count = 0;
+            if (node.val >= maxSeen)
+            {
+                count = 1;
+            }
+
+            maxSeen = Math.Max(node.val, maxSeen);
+
+            count += FindGoodNodes(node.left, maxSeen);
+            count += FindGoodNodes(node.right, maxSeen);
+
+            return count;
         }
     }
 }
---
+++ LeetCodeSolutionsTests/NeetCode/Trees/Good Nodes In Binary Tree/GoodNodesCounterTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Trees;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Good_Nodes_In_Binary_Tree.Tests
+{
+    [TestClass()]
+    public class GoodNodesCounterTests
+    {
+        [TestMethod()]
+        public void GoodNodesTest()
+        {
+            GoodNodesCounter.GoodNodes(
+                new TreeNode(2, new TreeNode(1, new TreeNode(3)), new TreeNode(1, new TreeNode(1), new TreeNode(5)))
+                ).Should().Be(3);
+        }
+
+        [TestMethod()]
+        public void GoodNodesTest2()
+        {
+            GoodNodesCounter.GoodNodes(
+                new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(-1))
+                ).Should().Be(4);
+        }
+    }
+}
