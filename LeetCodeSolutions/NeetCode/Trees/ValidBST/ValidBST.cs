feb15f4b32a9533cd28dd5fb9bc5250b9018f76c
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-31 10:34:24 +1000

    56/150 done

---
+++ LeetCodeSolutions/NeetCode/Trees/ValidBST/ValidBST.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeSolutions.NeetCode.Trees.ValidBST
+{
+    public class ValidBST
+    {
+        public static bool IsValidBST(TreeNode root)
+        {
+            bool TraverseBST(TreeNode node, int left, int right)
+            {
+                
+                if (node == null)
+                {
+                    return true;
+                }
+
+                if (left > node.val || node.val > right)
+                {
+                    return false;
+                }
+               
+                
+                return
+                    TraverseBST(node.left, left, node.val) 
+                    &&  
+                    TraverseBST(node.right, node.val, right);
+            }
+
+            return TraverseBST(root, int.MinValue, int.MaxValue);           
+
+        }       
+        
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Trees/ValidBST/ValidBSTTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeSolutions.NeetCode.Trees.ValidBST.Tests
+{
+    [TestClass()]
+    public class ValidBSTTests
+    {
+        [TestMethod()]
+        public void IsValidBSTTest()
+        {
+            ValidBST.IsValidBST(new TreeNode(2, new TreeNode(1), new TreeNode(3))).Should().BeTrue();
+        }
+
+        [TestMethod()]
+        public void IsInValidBSTTest()
+        {
+            ValidBST.IsValidBST(new TreeNode(1, new TreeNode(2), new TreeNode(3))).Should().BeFalse();
+        }
+
+
+        [TestMethod()]
+        public void IsInValidBSTTest2()
+        {
+            ValidBST.IsValidBST(new TreeNode(4, new TreeNode(3, new TreeNode(2, null, new TreeNode(4))))).Should().BeFalse();
+        }
+
+    }
+}
