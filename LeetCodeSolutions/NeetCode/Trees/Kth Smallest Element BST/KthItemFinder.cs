e69c2689408adf0ec657a375f136364701a1d973
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-31 11:21:04 +1000

    Kth Item

---
+++ LeetCodeSolutions/NeetCode/Trees/Kth Smallest Element BST/KthItemFinder.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeSolutions.NeetCode.Trees;
+
+namespace Kth_Smallest_Element_BST
+{
+    public class KthItemFinder
+    {
+        public static int KthSmallest(TreeNode root, int k)
+        {
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            TreeNode curr = root;
+
+            while (stack.Count > 0  || curr !=null)
+            {
+                while (curr != null)
+                {
+                    stack.Push(curr);
+                    curr = curr.left;
+                }
+
+                curr = stack.Pop();
+                k--;
+
+                if(k == 0)
+                {
+                    return curr.val;
+                }
+
+                curr = curr.right;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Trees/Kth Smallest Element BST/KthItemFinderTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Trees;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kth_Smallest_Element_BST.Tests
+{
+    [TestClass()]
+    public class KthItemFinderTests
+    {
+        [TestMethod()]
+        public void KthSmallestTest()
+        {
+            KthItemFinder.KthSmallest(
+                new TreeNode(
+                2, new TreeNode(1), new TreeNode(3)), 1).Should().Be(1);
+        }
+    }
+}
