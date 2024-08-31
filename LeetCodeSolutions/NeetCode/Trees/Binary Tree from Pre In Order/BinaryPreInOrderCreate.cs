f5a6b9092b7860219b7f34720e27508f04b517da
Arslan Qamar <arslan.qamar@outlook.com>
2024-08-31 16:50:59 +1000

    58/150

---
+++ LeetCodeSolutions/NeetCode/Trees/Binary Tree from Pre In Order/BinaryPreInOrderCreate.cs
@@ -0,0 +1,35 @@
+﻿using LeetCodeSolutions.NeetCode.Trees;
+
+namespace Binary_Tree_from_Pre_In_Order
+{
+    public class BinaryPreInOrderCreate
+    {
+
+        public static TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            
+            if(preorder == null || inorder == null || preorder.Length == 0 || inorder.Length == 0)
+            {
+                return null;
+            }
+
+            TreeNode root = new TreeNode(preorder[0]);
+
+            int index = Array.IndexOf(inorder, preorder[0]);
+
+            int[] leftElements = inorder.Take(index).ToArray();
+            int[] newLeftPreorder = preorder.Skip(1).Take(index).ToArray();
+
+
+            int[] rightElements = inorder.Skip(index + 1).ToArray();
+            int[] newRightPreorder = preorder.Skip(index + 1).ToArray();
+
+
+            root.left = BuildTree(newLeftPreorder, leftElements);
+            root.right = BuildTree(newRightPreorder, rightElements);
+
+            return root;
+        }
+
+    }
+}
