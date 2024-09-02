dba05f09c316a7bf21b53836e2830bec55ff22bb
Arslan Qamar <arslan.qamar@outlook.com>
2024-09-02 13:57:59 +1000

    Kth largest element

---
+++ LeetCodeSolutions/NeetCode/Heaps And Priority Queue/Kth Largest Element.cs
@@ -0,0 +1,38 @@
+﻿namespace Heaps_And_Priority_Queue
+{
+    public class KthLargest
+    {
+        private readonly int k;
+        private readonly int[] nums;
+        PriorityQueue<int, int> minHeap;
+
+        public KthLargest(int k, int[] nums)
+        {
+            this.k = k;
+            this.nums = nums;
+            minHeap = new PriorityQueue<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                minHeap.Enqueue(nums[i], nums[i]);
+                if (minHeap.Count > k)
+                {
+                    minHeap.Dequeue();
+                }
+            }
+        }
+
+        public int Add(int val)
+        {
+            minHeap.Enqueue(val, val);
+
+            if (minHeap.Count > k)
+            {
+                minHeap.Dequeue();
+            }
+
+            return minHeap.Peek();
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Heaps And Priority Queue/KthLargestTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Heaps_And_Priority_Queue.Tests
+{
+    [TestClass()]
+    public class KthLargestTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            KthLargest kthLargest = new KthLargest(3, new int[] { 1, 2, 3, 3 });
+
+            kthLargest.Add(3).Should().Be(3);
+            kthLargest.Add(5).Should().Be(3);
+            kthLargest.Add(6).Should().Be(3);
+            kthLargest.Add(7).Should().Be(5);
+            kthLargest.Add(8).Should().Be(6);
+        }
+    }
+}
