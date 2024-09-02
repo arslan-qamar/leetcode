77652a53f2b212d15763f45288dabb1a126ff304
Arslan Qamar <arslan.qamar@outlook.com>
2024-09-02 14:26:26 +1000

    62/150 done

---
+++ LeetCodeSolutions/NeetCode/Heaps And Priority Queue/Last Stone Weight/LastStoneWeightFinder.cs
@@ -0,0 +1,30 @@
+﻿namespace Last_Stone_Weight
+{
+    public class LastStoneWeightFinder
+    {
+        public static int LastStoneWeight(int[] stones)
+        {
+            if (stones == null || stones.Length == 0)
+                return 0;
+
+            PriorityQueue<int, int> maxHeap = new PriorityQueue<int, int>();
+
+            for (int i = 0; i < stones.Length; i++)
+            {
+                maxHeap.Enqueue(stones[i], -stones[i]);
+            }
+
+            while(maxHeap.Count > 1)
+            {
+                int firstStone = maxHeap.Dequeue();
+                int secondStone = maxHeap.Dequeue();
+
+                int remainingStone = firstStone - secondStone;
+                maxHeap.Enqueue(remainingStone, -remainingStone);
+                
+            }
+
+            return maxHeap.Peek();
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Heaps And Priority Queue/Last Stone Weight/LastStoneWeightFinderTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace Last_Stone_Weight.Tests
+{
+    [TestClass()]
+    public class LastStoneWeightFinderTests
+    {
+        [TestMethod()]
+        public void LastStoneWeightTest()
+        {
+            LastStoneWeightFinder.LastStoneWeight(new int[] { 2, 3, 6, 2, 4 }).Should().Be(1);
+        }
+
+
+        [TestMethod()]
+        public void LastStoneWeightTest2()
+        {
+            LastStoneWeightFinder.LastStoneWeight(new int[] { 1, 2 }).Should().Be(1);
+        }
+
+
+        [TestMethod()]
+        public void LastStoneWeightTest3()
+        {
+            LastStoneWeightFinder.LastStoneWeight(new int[] { 2, 2 }).Should().Be(0);
+        }
+    }
+}
