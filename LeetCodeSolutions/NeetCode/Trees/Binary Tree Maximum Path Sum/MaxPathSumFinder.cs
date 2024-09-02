e3df7b624eb17a17dbe1d37cfaeea0fa26d9bee4
Arslan Qamar <arslan.qamar@outlook.com>
2024-09-02 11:05:48 +1000

    leetcode 59/150

---
+++ LeetCodeSolutions/NeetCode/Trees/Binary Tree Maximum Path Sum/MaxPathSumFinder.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeSolutions.NeetCode.Trees;
+
+namespace Binary_Tree_Maximum_Path_Sum
+{
+    public class MaxPathSumFinder
+    {
+        public static int MaxPathSum(TreeNode root)
+        {
+            int DFS(TreeNode? node, ref int maxPath)
+            {
+                if (node == null)
+                {
+                    return 0;
+                }
+
+                var leftPath = Math.Max(DFS(node.left, ref maxPath), 0);
+                var rightPath = Math.Max(DFS(node.right, ref maxPath), 0);
+                
+                maxPath = Math.Max(maxPath, node.val + leftPath + rightPath);
+
+                return node.val + Math.Max(leftPath, rightPath);
+            }
+
+            int maxCount = root.val;
+            DFS(root, ref maxCount);
+            return maxCount;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Trees/Binary Tree Maximum Path Sum/MaxPathSumFinderTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Trees;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Binary_Tree_Maximum_Path_Sum.Tests
+{
+    [TestClass()]
+    public class MaxPathSumFinderTests
+    {
+        [TestMethod()]
+        public void MaxPathSumTest()
+        {
+            MaxPathSumFinder.MaxPathSum(
+                new TreeNode(1, new TreeNode(2), new TreeNode(3))).Should().Be(6);                        
+        }
+    }
+}
