e06b89552f902aeaf5481a873fdaf408313e7518
Arslan Qamar <arslan.qamar@outlook.com>
2024-09-02 11:46:12 +1000

    60/150 done

---
+++ LeetCodeSolutions/NeetCode/Trees/Serialise Deserialise Binary Tree/SerialiseDeserialise.cs
@@ -0,0 +1,48 @@
+﻿using LeetCodeSolutions.NeetCode.Trees;
+
+namespace Serialise_Deserialise_Binary_Tree
+{
+    public class SerialiseDeserialise
+    {
+        // Encodes a tree to a single string.
+        public static string Serialize(TreeNode? root)
+        {
+            if (root == null)
+                return "null";
+
+
+            string leftValue = Serialize(root.left);
+            string rightValue = Serialize(root.right);
+
+
+            return root.val + "," + leftValue + "," + rightValue;
+        }
+
+        // Decodes your encoded data to tree.
+        public static TreeNode? Deserialize(string data)
+        {
+            TreeNode? Deserialize(string[] data, ref int index)
+            {
+                if (data[index] == "null" || index >= data.Length)
+                {
+                    return null;
+                }
+
+                TreeNode node = new TreeNode(int.Parse(data[index]));
+
+                index = index + 1;
+                node.left = Deserialize(data,ref index);
+
+
+                index = index + 1;
+                node.right = Deserialize(data, ref index);
+
+
+                return node;
+            }
+
+            int idx = 0;
+            return Deserialize(data.Split(','), ref idx);
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Trees/Serialise Deserialise Binary Tree/SerialiseDeserialiseTests.cs
@@ -0,0 +1,63 @@
+﻿using Serialise_Deserialise_Binary_Tree;
+using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Trees;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Serialise_Deserialise_Binary_Tree.Tests
+{
+    [TestClass()]
+    public class SerialiseDeserialiseTests
+    {
+        [TestMethod()]
+        public void SerializeTest()
+        {
+            SerialiseDeserialise.Serialize(
+                new TreeNode(1, new TreeNode(2), new TreeNode(3))).Should().Be("1,2,null,null,3,null,null");
+        }
+
+        [TestMethod()]
+        public void SerializeTest2()
+        {
+            SerialiseDeserialise.Serialize(
+                new TreeNode(1)).Should().Be("1,null,null");
+        }
+
+
+        [TestMethod()]
+        public void SerializeTest3()
+        {
+            SerialiseDeserialise.Serialize(null).Should().Be("null");
+        }
+
+        [TestMethod()]
+        public void DeserializeTest()
+        {            
+            TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+         
+            string serialised = SerialiseDeserialise.Serialize(root);
+
+            SerialiseDeserialise.Deserialize(serialised).Should().BeEquivalentTo(root);
+        }
+
+        [TestMethod()]
+        public void DeserializeTest2()
+        {
+            TreeNode root = null;
+
+            string serialised = SerialiseDeserialise.Serialize(root);
+
+            SerialiseDeserialise.Deserialize(serialised).Should().BeEquivalentTo(root);
+        }
+
+        [TestMethod()]
+        public void DeserializeTest3()
+        {
+            TreeNode root = new TreeNode(1, new TreeNode(2));
+
+            string serialised = SerialiseDeserialise.Serialize(root);
+
+            SerialiseDeserialise.Deserialize(serialised).Should().BeEquivalentTo(root);
+        }
+
+    }
+}
