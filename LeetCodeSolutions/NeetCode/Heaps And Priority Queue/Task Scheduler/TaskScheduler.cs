64693ead4a32529e8a5c2a69101241b6bb79c376
Arslan Qamar <arslan.qamar@outlook.com>
2024-09-07 12:08:41 +1000

    65/150

---
+++ LeetCodeSolutions/NeetCode/Heaps And Priority Queue/Task Scheduler/TaskScheduler.cs
@@ -0,0 +1,59 @@
+﻿namespace Task_Scheduler
+{
+    public class TaskScheduler
+    {
+        public static int LeastInterval(char[] tasks, int n)
+        {
+            int result = 0;
+
+            int[] freq = new int[26];
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                freq[tasks[i] - 'A'] = ++freq[tasks[i] - 'A'];
+            }
+
+            PriorityQueue<int, int> maxPriority = new PriorityQueue<int, int>();
+            for (int i = 0; i < freq.Length; i++)
+            {
+                if (freq[i] > 0)
+                {
+                    maxPriority.Enqueue(freq[i], -freq[i]);
+                }
+            }
+            
+            while (maxPriority.Count > 0)
+            {
+
+                List<int> topFreq = new List<int>();
+                for (int i = 1; i <= n+1; i++)
+                {
+                    if (maxPriority.Count == 0)
+                    { break; }
+
+                    topFreq.Add(maxPriority.Dequeue() - 1);
+                }                
+
+                for (int i = 0; i < topFreq.Count; i++)
+                {
+                    if (topFreq[i] > 0)
+                    {
+                        maxPriority.Enqueue(topFreq[i], -topFreq[i]);
+                    }
+                }
+
+                if(maxPriority.Count > 0)
+                {
+                    result = result + n + 1;
+                }
+                else
+                {
+                    result = result + topFreq.Count;
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Heaps And Priority Queue/Task Scheduler/TaskSchedulerTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task_Scheduler.Tests
+{
+    [TestClass()]
+    public class TaskSchedulerTests
+    {
+        [TestMethod()]
+        public void LeastIntervalTest()
+        {
+            TaskScheduler.LeastInterval(new char[] { 'X', 'X', 'Y', 'Y' }, 2).Should().Be(5);
+        }
+
+        [TestMethod()]
+        public void LeastIntervalTest2()
+        {
+            TaskScheduler.LeastInterval(new char[] { 'A', 'A', 'A', 'B', 'C' }, 3).Should().Be(9);
+        }
+
+
+        [TestMethod()]
+        public void LeastIntervalTest3()
+        {
+            TaskScheduler.LeastInterval(new char[] { 'A', 'A', 'A', 'B', 'B', 'B', 'C', 'C', 'C', 'D', 'D', 'E' }, 2).Should().Be(12);
+        }
+
+    }
+}
