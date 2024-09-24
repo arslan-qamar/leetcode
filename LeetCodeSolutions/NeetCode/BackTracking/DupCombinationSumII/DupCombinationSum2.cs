e52833ad8edce93a0288c33a76a060b14c2ea857
Arslan Qamar <arslan.qamar@outlook.com>
2024-09-24 16:36:01 +1000

    DupCombination 72 done

---
+++ LeetCodeSolutions/NeetCode/BackTracking/DupCombinationSumII/DupCombinationSum2.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeSolutions.NeetCode.BackTracking.DupCombinationSumII
+{
+    public class DupCombinationSum2
+    {
+        public static List<List<int>> CombinationSum2(int[] candidates, int target)
+        {
+            List<List<int>> result = new List<List<int>>();
+
+            Array.Sort(candidates);
+
+            List<int> combination = new List<int>();
+
+            FindCombination(0, 0);
+
+            void FindCombination(int index, int sum)
+            {
+                if(sum == target)
+                {
+                    result.Add(new List<int>(combination));
+                    return;
+                }
+
+                if(index >= candidates.Length || sum > target)
+                {
+                    return;
+                }
+
+                sum += candidates[index];
+                combination.Add(candidates[index]);
+                FindCombination(index + 1, sum);
+
+
+                combination.RemoveAt(combination.Count - 1);
+                sum -= candidates[index];
+
+
+                while(index + 1 < candidates.Length && candidates[index] == candidates[index + 1])
+                {
+                    index++;
+                }
+
+                FindCombination(index + 1, sum);
+            }
+
+            return result;
+
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/BackTracking/DupCombinationSumII/DupCombinationSum2Tests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+
+namespace LeetCodeSolutions.NeetCode.BackTracking.DupCombinationSumII.Tests
+{
+    [TestClass()]
+    public class DupCombinationSum2Tests
+    {
+        [TestMethod()]
+        public void CombinationSum2Test()
+        {
+            DupCombinationSum2.CombinationSum2(new int[] { 9, 2, 2, 4, 6, 1, 5 }, 8).Should().BeEquivalentTo(
+
+                new List<List<int>>()
+                {
+                    new List<int>() {1,2,5 },
+                    new List<int>() {2,2,4 },
+                    new List<int>() {2,6 }
+                });
+        }
+    }
+}
