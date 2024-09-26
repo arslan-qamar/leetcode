f0227e40229f00765f3fbfd475a2a8b40988ae05
Arslan Qamar <arslan.qamar@outlook.com>
2024-09-26 16:08:03 +1000

    79/150 done

---
+++ LeetCodeSolutions/NeetCode/Trie/Prefix Tree/PrefixTree.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCodeSolutions.NeetCode.Trie
+{
+    public class PrefixTree
+    {
+        public class TrieNode
+        {
+            public bool End = false;
+
+            public Dictionary<char, TrieNode> map = new Dictionary<char, TrieNode>();
+        }
+
+        TrieNode root;
+
+        public PrefixTree()
+        {
+            root = new TrieNode();
+        }
+
+        public void Insert(string word)
+        {
+            TrieNode temp = root;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!temp.map.ContainsKey(word[i]))
+                {
+                    temp.map.Add(word[i], new TrieNode());
+                }
+
+                temp = temp.map[word[i]];
+            }
+            temp.End = true;
+        }
+
+        public bool Search(string word)
+        {
+            TrieNode temp = root;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!temp.map.ContainsKey(word[i]))
+                {
+                    return false;
+                }
+
+                temp = temp.map[word[i]];
+            }
+
+            return temp.End;
+        }
+
+        public bool StartsWith(string prefix)
+        {
+
+            TrieNode temp = root;
+            for (int i = 0; i < prefix.Length; i++)
+            {
+                if (!temp.map.ContainsKey(prefix[i]))
+                {
+                    return false;
+                }
+
+                temp = temp.map[prefix[i]];
+            }
+
+            return true;
+
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Trie/PrefixTree/PrefixTreeTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Trie;
+
+namespace PrefixTreeTests
+{
+    [TestClass()]
+    public class PrefixTreeTests
+    {
+
+        [TestMethod()]
+        public void SearchTest()
+        {
+            PrefixTree prefixTree = new PrefixTree();
+            prefixTree.Insert("dog");
+            prefixTree.Search("dog").Should().BeTrue();    // return true
+            prefixTree.Search("do").Should().BeFalse();     // return false
+            prefixTree.StartsWith("do").Should().BeTrue(); // return true
+            prefixTree.Insert("do");
+            prefixTree.Search("do").Should().BeTrue();     // return true
+        }
+
+    }
+}
