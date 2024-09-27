23fe76849d85d5a02703ffd4c8f561bca99dc099
Arslan Qamar <arslan.qamar@outlook.com>
2024-09-27 10:57:05 +1000

    Clone Graph

---
+++ LeetCodeSolutions/NeetCode/Graphs/Clone Graph/CloneGraphs.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeSolutions.NeetCode.Graphs.Clone_Graph
+{
+    public class CloneGraphs
+    {   
+        public static Node CloneGraph(Node root)
+        {
+            Dictionary<Node, Node> oldToNew = new Dictionary<Node, Node>();            
+            return Dfs(root);
+
+            Node Dfs(Node node)
+            {
+                if (node == null)
+                    return null;
+
+                if (oldToNew.ContainsKey(node))
+                    return oldToNew[node];
+
+                Node copy = new Node(node.val);
+                oldToNew[node] = copy;
+
+                foreach (Node nei in node.neighbors)
+                    copy.neighbors.Add(Dfs(nei));
+
+                return copy;
+            }
+        }        
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Graphs/Clone Graph/CloneGraphsTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using FluentAssertions.Equivalency;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace LeetCodeSolutions.NeetCode.Graphs.Clone_Graph.Tests
+{
+    [TestClass()]
+    public class CloneGraphsTests
+    {
+        [TestMethod()]
+        public void CloneGraphTest()
+        {
+            JsonSerializerSettings cycleIgnore = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+            Node node3 = new Node(3);
+
+            Node node11 =  new Node(1);
+            CloneGraphs.CloneGraph(node1).Should().BeEquivalentTo(new Node(1));
+
+            Node node22 = new Node(2);
+            node11.neighbors.Add(node22);
+            node1.neighbors.Add(node2);            
+            CloneGraphs.CloneGraph(node1).Should().BeEquivalentTo(node11);
+
+
+            EquivalencyAssertionOptions options = new EquivalencyAssertionOptions()
+                .IgnoringCyclicReferences().RespectingRuntimeTypes().ComparingByMembers<Node>();
+
+            node2.neighbors.Add(node1);
+            node22.neighbors.Add(node11);
+            JsonConvert.SerializeObject(CloneGraphs.CloneGraph(node1), cycleIgnore).Should().BeEquivalentTo(JsonConvert.SerializeObject(node11, cycleIgnore));
+
+            Node node33 = new Node(3);
+            node2.neighbors.Add(node3);
+            node22.neighbors.Add(node33);
+            JsonConvert.SerializeObject(CloneGraphs.CloneGraph(node1), cycleIgnore).Should().BeEquivalentTo(JsonConvert.SerializeObject(node11, cycleIgnore));
+
+
+            node3.neighbors.Add(node2);
+            node33.neighbors.Add(node22);
+            JsonConvert.SerializeObject(CloneGraphs.CloneGraph(node1), cycleIgnore).Should().BeEquivalentTo(JsonConvert.SerializeObject(node11, cycleIgnore));
+
+
+        }
+    }
+}
