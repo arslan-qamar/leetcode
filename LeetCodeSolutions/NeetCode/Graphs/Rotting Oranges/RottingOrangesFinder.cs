f9f9e6649b3dd37a7aab0fbb2a22fae0b843c0e7
Arslan Qamar <arslan.qamar@outlook.com>
2024-09-28 13:40:11 +1000

    85/150 Done

---
+++ LeetCodeSolutions/NeetCode/Graphs/Rotting Oranges/RottingOrangesFinder.cs
@@ -0,0 +1,79 @@
+﻿namespace LeetCodeSolutions.NeetCode.Graphs.Rotting_Oranges
+{
+    public class RottingOrangesFinder
+    {
+        public int OrangesRotting(int[][] grid)
+        {
+            int result = 0;
+
+            HashSet<(int Row, int Col)> visited = new HashSet<(int Row, int Col)>();
+
+            Queue<(int Row, int Col)> bfsQueue = new Queue<(int Row, int Col)>();
+
+            int freshFruit = 0;
+            for (int r = 0; r < grid.Length; r++)
+            {
+                for (int c = 0; c < grid[0].Length; c++)
+                {
+                    if (grid[r][c] == 1)
+                    {
+                        freshFruit++;
+                    }
+
+                    if (grid[r][c]== 2)
+                    {
+                        bfsQueue.Enqueue((r, c));
+                        visited.Add((r, c));
+                    }
+                }
+            }
+
+            
+            while(bfsQueue.Count > 0)
+            {
+                int itemsInQueue = bfsQueue.Count;
+                for (int i = 0; i < itemsInQueue; i++)
+                {
+                    var entry = bfsQueue.Dequeue();
+
+                    UpdateNeighbourFruits(entry.Row, entry.Col + 1);
+                    UpdateNeighbourFruits(entry.Row, entry.Col - 1);
+                    UpdateNeighbourFruits(entry.Row + 1, entry.Col);
+                    UpdateNeighbourFruits(entry.Row - 1, entry.Col);
+
+                }
+
+                if(bfsQueue.Count > 0)
+                {
+                    if (result == -1)
+                    {
+                        result = 1;                     
+                    }
+                    else
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            void UpdateNeighbourFruits(int r, int c)
+            {
+                if (r < 0 || r >= grid.Length || c < 0 || c >= grid[0].Length || grid[r][c] == 0 || visited.Contains((r, c)))
+                {
+                    return;
+                }
+
+                if (grid[r][c] == 1)
+                {
+                    freshFruit--;
+                    grid[r][c] = grid[r][c] + 1;                
+                    bfsQueue.Enqueue((r, c));
+                    visited.Add((r, c));
+                }
+            }
+
+            return freshFruit == 0 ? result : -1;
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Graphs/Rotting Oranges/RottingOrangesFinderTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Graphs.Rotting_Oranges;
+
+namespace Rotting_Oranges_Tests
+{
+    [TestClass()]
+    public class RottingOrangesFinderTests
+    {
+        [TestMethod()]
+        public void OrangesRottingTest()
+        {
+
+            RottingOrangesFinder rf = new RottingOrangesFinder();
+
+            rf.OrangesRotting(new int[][]
+            {
+                [1,1,0],
+                [0,1,1],
+                [0,1,2]
+            }).Should().Be(4);            
+        }
+
+        [TestMethod()]
+        public void OrangesRottingTest2()
+        {
+
+            RottingOrangesFinder rf = new RottingOrangesFinder();
+
+            rf.OrangesRotting(new int[][]
+            {
+                [1,0,1],[0,2,0],[1,0,1]
+            }).Should().Be(-1);
+        }
+
+
+        [TestMethod()]
+        public void OrangesRottingTestNeetCodeFailed()
+        {
+
+            RottingOrangesFinder rf = new RottingOrangesFinder();
+
+            rf.OrangesRotting(new int[][]
+            {
+                [2,1,1],
+                [0,1,1],
+                [1,0,1]
+            }).Should().Be(-1);
+        }
+
+        [TestMethod()]
+        public void OrangesRottingTestNeetCodeFailed2()
+        {
+
+            RottingOrangesFinder rf = new RottingOrangesFinder();
+
+            rf.OrangesRotting(new int[][]
+            {
+                [0, 2]
+            }).Should().Be(0);
+        }
+        
+
+    }
+}
