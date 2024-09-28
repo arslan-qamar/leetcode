1f83e05561d04d2a367108b84437d5a6fe326d93
Arslan Qamar <arslan.qamar@outlook.com>
2024-09-29 08:40:22 +1000

    87 / 150 done

---
+++ LeetCodeSolutions/NeetCode/Graphs/Surrounded Region/Surrounded.cs
@@ -0,0 +1,63 @@
+﻿namespace LeetCodeSolutions.NeetCode.Graphs.Surrounded_Region
+{
+    public class Surrounded
+    {
+        public void Solve(char[][] board)
+        {
+
+            for (int r = 0; r < board.Length; r++)
+            {
+                for (int c = 0; c < board[0].Length; c++)
+                {
+
+                    if (board[r][c] == 'O' && (r == 0 || r == board.Length - 1 || c == 0 || c == board[0].Length - 1))
+                    {
+                        Dfs(r, c);
+                    }
+                }
+            }
+
+            for (int r = 0; r < board.Length; r++)
+            {
+                for (int c = 0; c < board[0].Length; c++)
+                {
+
+                    if (board[r][c] == 'O')
+                    {
+                        board[r][c] = 'X';
+                    }
+                }
+            }
+
+            for (int r = 0; r < board.Length; r++)
+            {
+                for (int c = 0; c < board[0].Length; c++)
+                {
+
+                    if (board[r][c] == 'T')
+                    {
+                        board[r][c] = 'O';
+                    }
+                }
+            }
+
+
+            void Dfs(int row, int col)
+            {
+                if(row < 0 || row >= board.Length || col < 0 || col >= board[0].Length || board[row][col] != 'O')
+                {
+                    return;
+                }
+
+                
+                board[row][col] = 'T';                    
+                
+
+                Dfs(row + 1, col);
+                Dfs(row - 1, col);
+                Dfs(row, col + 1);
+                Dfs(row, col - 1);                
+            }
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Graphs/Surrounded Region/SurroundedTests.cs
@@ -0,0 +1,106 @@
+﻿using FluentAssertions;
+
+namespace LeetCodeSolutions.NeetCode.Graphs.Surrounded_Region.Tests
+{
+    [TestClass()]
+    public class SurroundedTests
+    {
+        [TestMethod()]
+        public void SolveTest()
+        {
+            char[][] board = [
+                              ['X','X','X','X'],
+                              ['X','O','O','X'],
+                              ['X','O','O','X'],
+                              ['X','X','X','O']
+                             ];
+
+
+            Surrounded s = new Surrounded();
+
+            s.Solve(board);
+
+
+            board.Should().BeEquivalentTo(
+
+                new char[][]{
+                              ['X','X','X','X'],
+                              ['X','X','X','X'],
+                              ['X','X','X','X'],
+                              ['X','X','X','O']
+                }
+
+                );
+
+        }
+
+
+        [TestMethod()]
+        public void SolveTest2()
+        {
+            char[][] board = [
+                              ['X','X','X','X'],
+                              ['X','O','X','X'],
+                              ['X','X','X','X'],
+                              ['X','X','X','O']
+                             ];
+
+
+            Surrounded s = new Surrounded();
+
+            s.Solve(board);
+
+
+            board.Should().BeEquivalentTo(
+
+                new char[][]{
+                              ['X','X','X','X'],
+                              ['X','X','X','X'],
+                              ['X','X','X','X'],
+                              ['X','X','X','O']
+                }
+
+                );
+
+        }
+
+
+
+        [TestMethod()]
+        public void SolveTestNeetCode()
+        {
+            char[][] board = [
+                ['O', 'X', 'X', 'O', 'X'],
+                ['X', 'O', 'O', 'X', 'O'],
+                ['X', 'O', 'X', 'O', 'X'],
+                ['O', 'X', 'O', 'O', 'O'],
+                ['X', 'X', 'O', 'X', 'O']];
+
+
+            Surrounded s = new Surrounded();
+
+            s.Solve(board);
+
+
+            //['O', 'X', 'X', 'O', 'X'],
+            //['X', 'X', 'X', 'X', 'O'],
+            //['X', 'X', 'X', 'X', 'X'],
+            //['O', 'X', 'O', 'O', 'O'],
+            //['X', 'X', 'O', 'X', 'O']
+                
+                
+            board.Should().BeEquivalentTo(
+
+            new char[][]{
+
+                    ['O','X','X','O','X'],
+                    ['X','X','X','X','O'],
+                    ['X','X','X','O','X'],
+                    ['O','X','O','O','O'],
+                    ['X','X','O','X','O']
+                    });
+        }
+
+    }
+
+}
