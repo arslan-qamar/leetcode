2de94f0fbdc37c195b9f8167394506f2be3bd7f7
Arslan Qamar <arslan.qamar@outlook.com>
2024-10-01 18:31:04 +1000

    92/150 done

---
+++ LeetCodeSolutions/NeetCode/Graphs/Redundant Connection/RedundantConnection.cs
@@ -0,0 +1,72 @@
+﻿namespace LeetCodeSolutions.NeetCode.Graphs.Redundant_Connection
+{
+    public class RedundantConnection
+    {
+
+        public int[] FindRedundantConnection(int[][] edges)
+        {
+            int[] result = new int[0];
+
+
+            int[] parent = new int[edges.Length+1];
+            int[] rank = new int[edges.Length+1];
+
+            for (int i = 0; i <= edges.Length; i++)
+            {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+
+            foreach (var edge in edges)
+            {
+                int p1 = FindParent(edge[0]);
+                int p2 = FindParent(edge[1]);
+
+                if(Union(p1,p2) == 0)
+                {
+                    return edge;
+                }
+            }
+
+            int Union(int p1, int p2)
+            {
+                if(p1 == p2)
+                {
+                    return 0;
+                }
+
+                if (rank[p1] > rank[p2])
+                {
+                    parent[p2] = p1;
+                    rank[p1] += rank[p2];
+                }
+                else
+                {
+                    parent[p1] = p2;
+                    rank[p2] += rank[p1];
+                }
+
+                return 1;
+
+            }
+
+            int FindParent(int i)
+            {
+                int res = i;
+
+                while (parent[res] != res)
+                {
+                    parent[res] = parent[parent[res]];
+                    res = parent[res];
+                }
+
+                return parent[res];
+            }
+
+            return result;
+        }
+
+
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Graphs/Redundant Connection/RedundantConnectionTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+namespace LeetCodeSolutions.NeetCode.Graphs.Redundant_Connection.Tests
+{
+    [TestClass()]
+    public class RedundantConnectionTests
+    {
+        [TestMethod()]
+        public void FindRedundantConnectionTest()
+        {
+            RedundantConnection rc = new RedundantConnection();
+
+            rc.FindRedundantConnection([[1, 2], [1, 3], [3, 4], [2, 4]]).Should().BeEquivalentTo(new int[] { 2, 4 });
+        }
+
+        [TestMethod()]
+        public void FindRedundantConnectionTest2()
+        {
+            RedundantConnection rc = new RedundantConnection();
+
+            rc.FindRedundantConnection([[1, 2], [1, 3], [1, 4], [3, 4], [4, 5]]).Should().BeEquivalentTo(new int[] { 3, 4 });
+        }
+    }
+}
