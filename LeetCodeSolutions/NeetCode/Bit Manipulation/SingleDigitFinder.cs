c215f95e1c08c19ec12684faaa1529dc08d91266
Arslan Qamar <arslan.qamar@outlook.com>
2024-10-03 13:49:12 +1000

    94/150 done

---
+++ LeetCodeSolutions/NeetCode/Bit Manipulation/SingleDigitFinder.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeSolutions.NeetCode.Bit_Manipulation
+{
+    public class SingleDigitFinder
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+
+            foreach (var n in nums)
+            {
+                result ^= n;
+            }
+
+            return result;
+        }
+    }
+}
