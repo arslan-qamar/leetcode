8ed1c07860da798ab651ac3fafb75cc0d5d4b91f
Arslan Qamar <arslan.qamar@outlook.com>
2024-10-03 17:06:19 +1000

    95/150 done

---
+++ LeetCodeSolutionsTests/NeetCode/Bit Manipulation/Number of 1s/NumberOfOnesTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Bit_Manipulation.Number_of_1s;
+
+namespace Number_of_1s.Tests
+{
+    [TestClass()]
+    public class NumberOfOnesTests
+    {
+        [TestMethod()]
+        public void HammingWeightTest()
+        {
+            NumberOfOnes n1 = new NumberOfOnes();
+
+            n1.HammingWeight(23).Should().Be(4);
+        }
+    }
+}
