26793351b7246ca5fb3c3101c61a11f1d46287b1
Arslan Qamar <arslan.qamar@outlook.com>
2024-10-04 09:13:46 +1000

    Bit Reverser

---
+++ LeetCodeSolutions/NeetCode/Bit Manipulation/ReverseBits/ReverserBits.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeSolutions.NeetCode.Bit_Manipulation.ReverseBits
+{
+    public class ReverserBits
+    {
+        public uint ReverseBits(uint n)
+        {
+            uint result = 0;
+
+            for (int i = 0; i < 31; i++)
+            {
+
+                uint bit = (n >> i) & 1;
+
+                result += (bit << (31 - i));
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Bit Manipulation/ReverseBits/ReverserBitsTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Bit_Manipulation.ReverseBits;
+
+namespace ReverseBits.Tests
+{
+    [TestClass()]
+    public class ReverserBitsTests
+    {
+        [TestMethod()]
+        public void ReverseBitsTest()
+        {
+            ReverserBits rb = new ReverserBits();
+            rb.ReverseBits(21).Should().Be(2818572288);
+
+        }
+    }
+}
