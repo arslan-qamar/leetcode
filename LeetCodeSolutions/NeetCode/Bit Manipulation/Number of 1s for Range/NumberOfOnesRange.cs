f16aea1d90d6a5d9e36e2eb43b5ba33845c259c3
Arslan Qamar <arslan.qamar@outlook.com>
2024-10-04 08:55:05 +1000

    96/150 done

---
+++ LeetCodeSolutions/NeetCode/Bit Manipulation/Number of 1s for Range/NumberOfOnesRange.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCodeSolutions.NeetCode.Bit_Manipulation.Number_of_1s_for_Range
+{
+    public class NumberOfOnesRange
+    {
+
+        public int[] CountBits(int n)
+        {
+            int[] dp = new int[n + 1];
+
+            int offset = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (offset * 2 == i)
+                {
+                    offset = i;
+                }
+
+                dp[i] = 1 + dp[i - offset];
+            }
+            return dp;
+        }
+
+        public int[] CountBitsHammingWeight(int n)
+        {
+            if (n == 0)
+            {
+                return new int[] { 0 };
+            }
+
+            int[] res = new int[n + 1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                int num = i;
+                int count = 0;
+                while(num !=0)
+                {
+                    num = num & (num - 1);
+                    count++;
+                }
+
+                res[i] = count;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Bit Manipulation/Number of 1s for Range/NumberOfOnesRangeTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Bit_Manipulation.Number_of_1s_for_Range;
+
+namespace Number_of_1s_for_Range.Tests
+{
+    [TestClass()]
+    public class NumberOfOnesRangeTests
+    {
+        [TestMethod()]
+        public void CountBitsTest()
+        {
+            NumberOfOnesRange noo = new NumberOfOnesRange();
+
+            noo.CountBits(4).Should().BeEquivalentTo(new int[] { 0, 1, 1, 2, 1 }, options => options.WithStrictOrdering());
+        }
+
+    }
+}
