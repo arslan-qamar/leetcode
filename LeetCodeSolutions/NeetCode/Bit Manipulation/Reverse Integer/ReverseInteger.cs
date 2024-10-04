a7c3d417342ca85b229c8f0e4b769d4fa20462f1
Arslan Qamar <arslan.qamar@outlook.com>
2024-10-04 15:36:03 +1000

    100/150 done , yes

---
+++ LeetCodeSolutions/NeetCode/Bit Manipulation/Reverse Integer/ReverseInteger.cs
@@ -0,0 +1,29 @@
+﻿namespace Reverse_Integer
+{
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            int result = 0;
+            int num = x;
+
+            while (num != 0)
+            {
+                int digit = num % 10;
+                num = num / 10;
+
+                if (
+                    (result > int.MaxValue / 10 || (result == int.MaxValue / 10 && digit > int.MaxValue % 10)) ||
+                    (result < int.MinValue / 10 || (result == int.MinValue / 10 && digit > int.MinValue % 10))
+                   )
+                {
+                    return 0;
+                }
+
+                result = result * 10 + digit;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Bit Manipulation/Reverse Integer/ReverseIntegerTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+
+namespace Reverse_Integer.Tests
+{
+    [TestClass()]
+    public class ReverseIntegerTests
+    {
+        [TestMethod()]
+        public void ReverseTest()
+        {
+            ReverseInteger ri = new ReverseInteger();
+            ri.Reverse(123456).Should().Be(654321);
+        }
+
+        [TestMethod()]
+        public void ReverseTestNC1()
+        {
+            ReverseInteger ri = new ReverseInteger();
+            ri.Reverse(-1234).Should().Be(-4321);
+        }
+
+        [TestMethod()]
+        public void ReverseTestNC2()
+        {
+            ReverseInteger ri = new ReverseInteger();
+            ri.Reverse(1234236467).Should().Be(0);
+        }
+
+    }
+}
