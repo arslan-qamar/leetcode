8638ac321c09718bfafcaadec419a938eb57eb61
Arslan Qamar <arslan.qamar@outlook.com>
2024-10-04 14:07:19 +1000

    98/150 done

---
+++ LeetCodeSolutions/NeetCode/Bit Manipulation/Missing Number/MissingNumberFinder.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeSolutions.NeetCode.Bit_Manipulation.Missing_Number
+{
+    public class MissingNumberFinder
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int xor1 = 0, xor2 = 0;
+
+            // XOR all array elements
+            for (int i = 0; i < nums.Length; i++)
+            {
+                xor2 ^= nums[i];
+            }
+
+            // XOR all numbers from 1 to n
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                xor1 ^= i;
+            }
+
+            // Missing number is the XOR of xor1 and xor2
+            return xor1 ^ xor2;
+        }
+
+        public int MissingNumberGuass(int[] nums)
+        {
+            int result = (nums.Length * (nums.Length + 1)) / 2;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result = result - nums[i];
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Bit Manipulation/Missing Number/MissingNumberFinderTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+
+namespace LeetCodeSolutions.NeetCode.Bit_Manipulation.Missing_Number.Tests
+{
+    [TestClass()]
+    public class MissingNumberFinderTests
+    {
+        [TestMethod()]
+        public void MissingNumberTest()
+        {
+            MissingNumberFinder mf = new MissingNumberFinder();
+
+            mf.MissingNumber([1, 2, 3]).Should().Be(0);
+        }
+
+        [TestMethod()]
+        public void MissingNumberTest2()
+        {
+            MissingNumberFinder mf = new MissingNumberFinder();
+
+            mf.MissingNumber([0, 2]).Should().Be(1);
+        }
+
+        [TestMethod()]
+        public void MissingNumberTest3()
+        {
+            MissingNumberFinder mf = new MissingNumberFinder();
+
+            mf.MissingNumber([1, 2]).Should().Be(0);
+        }
+    }
+
+}
