bf4d1895e9116296b119b8f4400b472d4afeba73
Arslan Qamar <arslan.qamar@outlook.com>
2024-10-07 10:48:40 +1100

    103/150

---
+++ LeetCodeSolutions/NeetCode/Advance Graphs/Cheapest Flight Path/CheapestFlightPath.cs
@@ -0,0 +1,99 @@
+﻿namespace LeetCodeSolutions.NeetCode.Advance_Graphs.Cheapest_Flight_Path
+{
+    public class CheapestFlightPath
+    {
+        public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int k)
+        {
+            //BellmanFord Algorithm
+            int[] prices = new int[n];
+            Array.Fill(prices, int.MaxValue);
+
+            prices[src] = 0;
+
+            for (int i = 0; i <= k; i++)
+            {
+                int[] tmp = (int[])prices.Clone();
+                foreach (var flight in flights)
+                {
+                    int s = flight[0];
+                    int d = flight[1];
+                    int p = flight[2];
+
+                    if (prices[s] == int.MaxValue)
+                    {
+                        continue;
+                    }
+
+                    tmp[d] = Math.Min(tmp[d], prices[s] + p);
+                }
+                prices = tmp;
+            }
+
+            return prices[dst] == int.MaxValue ? -1 : prices[dst];
+        }
+
+
+
+        public int FindCheapestPriceDjikstra(int n, int[][] flights, int src, int dst, int k)
+        {
+            int cost = int.MaxValue;
+
+            Dictionary<int, List<(int Price, int Node)>> adjList = new Dictionary<int, List<(int Price, int Node)>>();
+
+            foreach (var flight in flights)
+            {
+                int from = flight[0];
+                int to = flight[1];
+                int price = flight[2];
+
+                if(!adjList.ContainsKey(from))
+                {
+                    adjList[from] = new List<(int Price, int Node)>();
+                }
+
+                adjList[from].Add((price, to));
+            }
+
+
+            if (!adjList.ContainsKey(src))
+            {
+                return -1;
+            }
+
+            SortedSet<(int Cost, int Node, int hops)> minHeap = new SortedSet<(int Cost, int Node, int hops)>();
+            minHeap.Add((0, src, 0));
+            HashSet<int> visited = new HashSet<int>();
+
+            
+            while(minHeap.Count  > 0)
+            {
+                var stop = minHeap.Min;
+                minHeap.Remove(stop);
+
+                if(stop.Node == dst)
+                {
+                    cost = Math.Min(cost, stop.Cost);
+                }
+                
+                if (stop.hops <= k)
+                {
+                    if(adjList.ContainsKey(stop.Node))
+                    {
+
+                        foreach (var nextStop in adjList[stop.Node])
+                        {
+                            if (stop.hops <= k)
+                            {
+                                minHeap.Add((stop.Cost + nextStop.Price, nextStop.Node, stop.hops + 1));
+                            }
+                        }
+                    }
+                }
+            }
+
+            return cost == int.MaxValue ? -1 : cost;
+
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Advance Graphs/Cheapest Flight Path/CheapestFlightPathTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Advance_Graphs.Cheapest_Flight_Path;
+
+namespace Cheapest_Flight_Path.Tests
+{
+    [TestClass()]
+    public class CheapestFlightPathTests
+    {
+        [TestMethod()]
+        public void FindCheapestPriceTest()
+        {
+            CheapestFlightPath cp = new CheapestFlightPath();
+
+            cp.FindCheapestPrice(4, [[0, 1, 200], [1, 2, 100], [1, 3, 300], [2, 3, 100]], 0, 3, 1).Should().Be(500);
+        }
+
+    }
+}
