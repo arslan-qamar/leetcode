eb0be97e9bac7a27135a618dc084e3e885e46454
Arslan Qamar <arslan.qamar@outlook.com>
2024-10-06 18:57:11 +1100

    102/150

---
+++ LeetCodeSolutions/NeetCode/Advance Graphs/Network Delay/NetworkDelay.cs
@@ -0,0 +1,59 @@
+﻿namespace LeetCodeSolutions.NeetCode.Advance_Graphs.Network_Delay
+{
+    public class NetworkDelay
+    {
+        public int NetworkDelayTime(int[][] times, int n, int k)
+        {
+            int minTime = 0;
+
+            Dictionary<int, List<(int Time, int Node)>> adjList = new Dictionary<int, List<(int Time, int Node)>>();
+
+            foreach (var item in times)
+            {
+                int source = item[0];
+                int dest = item[1];
+                int time = item[2];
+
+                if (!adjList.ContainsKey(source))
+                {
+                    adjList[source] = new List<(int Time, int Node)>();
+                }
+                adjList[source].Add((time, dest));
+
+            }
+
+            HashSet<int> visited = new HashSet<int>();
+            SortedSet<(int Time, int Node)> minHeap = new SortedSet<(int Time, int Node)>();
+            minHeap.Add((0, k));
+
+
+            while (minHeap.Count > 0)
+            {
+                var currNode = minHeap.Min;
+                minHeap.Remove(currNode);
+
+                if (visited.Contains(currNode.Node))
+                {
+                    continue;
+                }
+
+                visited.Add(currNode.Node);
+                minTime = Math.Max(minTime, currNode.Time);
+
+                if (adjList.ContainsKey(currNode.Node))
+                {
+                    foreach (var neiNode in adjList[currNode.Node])
+                    {
+                        if (!visited.Contains(neiNode.Node))
+                        {                            
+                            minHeap.Add((neiNode.Time + currNode.Time, neiNode.Node));
+                        }
+                    }
+                }
+            }
+
+            return visited.Count == n ? minTime : -1;
+        }
+
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Advance Graphs/Network Delay/NetworkDelayTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Advance_Graphs.Network_Delay;
+
+namespace Network_Delay.Tests
+{
+    [TestClass()]
+    public class NetworkDelayTests
+    {
+        [TestMethod()]
+        public void NetworkDelayTimeTest()
+        {
+            NetworkDelay nd = new NetworkDelay();
+            nd.NetworkDelayTime([[1, 2, 1], [2, 3, 1], [1, 4, 4], [3, 4, 1]], 4, 1).Should().Be(3);
+        }
+
+        [TestMethod()]
+        public void NetworkDelayTimeTest2()
+        {
+            NetworkDelay nd = new NetworkDelay();
+            nd.NetworkDelayTime([[1, 2, 1], [2, 3, 1]], 3, 2).Should().Be(-1);
+        }
+
+
+        [TestMethod()]
+        public void NetworkDelayTimeTestNCFail()
+        {
+            NetworkDelay nd = new NetworkDelay();
+            nd.NetworkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2).Should().Be(2);
+        }
+
+
+        [TestMethod()]
+        public void NetworkDelayTimeTestNCFail2()
+        {
+            NetworkDelay nd = new NetworkDelay();
+            nd.NetworkDelayTime([[1, 2, 1], [2, 1, 3]], 2, 2).Should().Be(3);
+        }
+
+        [TestMethod()]
+        public void NetworkDelayTimeTestNCFail3()
+        {
+            NetworkDelay nd = new NetworkDelay();
+            nd.NetworkDelayTime([[2, 4, 10], [5, 2, 38], [3, 4, 33], [4, 2, 76], [3, 2, 64], [1, 5, 54], [1, 4, 98], [2, 3, 61], [2, 1, 0], [3, 5, 77], [5, 1, 34], [3, 1, 79], [5, 3, 2], [1, 2, 59], [4, 3, 46], [5, 4, 44], [2, 5, 89], [4, 5, 21], [1, 3, 86], [4, 1, 95]], 5, 1).Should().Be(69);
+        }
+
+
+    }
+}
