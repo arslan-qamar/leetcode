58feeb2f6d387d32c7130397ef0bbbd49b61564f
Arslan Qamar <arslan.qamar@outlook.com>
2024-10-16 20:18:54 +1100

    110/150 done

---
+++ LeetCodeSolutions/NeetCode/Dynamic Programming/House Robber II/HouseRobber.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeSolutions.NeetCode.Dynamic_Programming.House_Robber_II
+{
+    public class HouseRobber
+    {
+        public int Rob(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            int maxRob = 0;
+
+            int rob1 = 0;
+            int rob2 = 0;
+
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                int temp = Math.Max(rob1 + nums[i], rob2);
+                rob1 = rob2;
+                rob2 = temp;
+            }
+
+            maxRob = Math.Max(maxRob, rob2);
+            rob1 = 0;
+            rob2 = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int temp = Math.Max(rob1 + nums[i], rob2);
+                rob1 = rob2;
+                rob2 = temp;
+            }
+            maxRob = Math.Max(maxRob, rob2);
+
+            return maxRob;
+        }
+    }
+}
---
+++ LeetCodeSolutionsTests/NeetCode/Dynamic Programming/House Robber II/HouseRobberTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using LeetCodeSolutions.NeetCode.Dynamic_Programming.House_Robber_II;
+
+namespace House_Robber_II.Tests
+{
+    [TestClass()]
+    public class HouseRobberTests
+    {
+        [TestMethod()]
+        public void RobTest()
+        {
+            HouseRobber hr = new HouseRobber();
+
+            hr.Rob([3, 4, 3]).Should().Be(4);
+        }
+    }
+}
